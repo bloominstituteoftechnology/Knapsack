17ea1a2957631871b1d5e7ae60b6def3dd818c07
soycode <soycode@users.noreply.github.com>
2017-12-04 08:11:08 -0800

    Add exhaustive solution

--- solver.py
+++ solver.py
@@ -5,8 +5,47 @@ from collections import namedtuple
 
 Item = namedtuple("Item", ['index', 'size', 'value'])
 
-# Incorrect but feasible and efficient implementation of the knapsack problem
+def knapsackExhaustive(items, capacity):
+    """
+    Correct but generally infeasible/inefficient at scale knapsack solution.
+    """
+    # Recursively check all combinations of items with inner helper method
+    def knapsackRec(items, capacity, value, taken):
+        if not items or (len(items) == 1 and items[0].size > capacity):
+            # No remaining items that fit
+            return value, taken
+        elif len(items) == 1:
+            # Last item and it fits, take it
+            taken[items[0].index - 1] = 1
+            value += items[0].value
+            return value, taken
+        elif capacity > items[0].size:
+            # Recurse cases of taking item/not taking item, return max
+            new_taken = taken[:]  # Copy to avoid marking everything taken
+            new_taken[items[0].index - 1] = 1
+            return max(
+                knapsackRec(items[1:], capacity - items[0].size,
+                            value + items[0].value, new_taken),
+                knapsackRec(items[1:], capacity, value, taken),
+                key=lambda tup: tup[0]
+            )
+        else:
+            # Doesn't fit, just skip and recurse on rest
+            return knapsackRec(items[1:], capacity, value, taken)
+    # Initial call with nothing taken
+    value, taken = knapsackRec(items, capacity, 0, [0] * len(items))
+    # Output is a string of the value of the knapsack (line 1)
+    # Note - first row is all 0s because picking from 0 items
+    # Need to have it to refer back to for solution computation
+    # and a 0/1 array indicating whether items were taken (line 2)
+    output_data = str(value) + '\n'
+    output_data += ' '.join(map(str, taken))
+    return output_data
+
 def knapsackGreedy(items, capacity):
+    """
+    Incorrect but feasible and efficient solution for the knapsack problem.
+    """
     value = 0
     weight = 0
     taken = [0] * len(items)
@@ -22,16 +61,15 @@ def knapsackGreedy(items, capacity):
             value += item.value
             weight += (item.size * item.value)
 
-    # Output is a string of the value of the knapsack (line 1)
-    # and a 0/1 array indicating whether items were taken (line 2)
     output_data = str(value) + '\n'
     output_data += ' '.join(map(str, taken))
     return output_data
 
-# Correct but very memory inefficient solution, can only solve
-# problems where computer memory is at least number of items times capacity
-# time 8 bites large
 def knapsackDP(items, capacity):
+    """
+    Correct but very memory inefficient solution, can only solve problems where
+    computer memory is at least number of items times capacity time 8bytes large
+    """
     # Initialize the knapsack matrix: rows are items (including a "no item" row)
     # Columns are integral capacities: 0, 1, 2, ..., capacity - 1, capacity
     knapsack = [[0] * (capacity + 1) for _ in range(len(items) + 1)]
@@ -58,7 +96,7 @@ def knapsackDP(items, capacity):
                     knapsack[i-1][j - items[i-1].size] + items[i-1].value)
 
     # With this loop we can traverse the matrix and figure out what items
-    # to take in opur knapsack, that is, if the value increased from item i-1
+    # to take in our knapsack, that is, if the value increased from item i-1
     # to item i then we know we took item it and should go to the previous
     # item in the smaller knapsack
     # (the column discounting the size of the taken item)
@@ -72,19 +110,17 @@ def knapsackDP(items, capacity):
         else:
             i -= 1
 
-    # Output is a string of the value of the knapsack,
-    # which is the last value in the knapsack matrix (line 1)
-    # and a 0/1 array indicating whether items were taken,
-    # which is the matrix traversal done above (line 2)
     output_data = str(knapsack[-1][-1]) + '\n'
     output_data += ' '.join(map(str, taken))
     return output_data
 
-# This would be cool with breath-first search, pass for now
-# Idea: https://ocw.mit.edu/courses/civil-and-environmental-engineering/1-204-computer-algorithms-in-systems-engineering-spring-2010/lecture-notes/MIT1_204S10_lec16.pdf
-# More: http://www.mathcs.emory.edu/~cheung/Courses/323/Syllabus/BranchBound/Docs/Knapsack-branch+bound.pdf
-# C++: https://stackoverflow.com/questions/11498035/c-implementation-of-knapsack-branch-and-bound
 def knapsackBB(items):
+    """
+    This would be cool with breath-first search, pass for now
+    Idea: https://ocw.mit.edu/courses/civil-and-environmental-engineering/1-204-computer-algorithms-in-systems-engineering-spring-2010/lecture-notes/MIT1_204S10_lec16.pdf
+    More: http://www.mathcs.emory.edu/~cheung/Courses/323/Syllabus/BranchBound/Docs/Knapsack-branch+bound.pdf
+    C++: https://stackoverflow.com/questions/11498035/c-implementation-of-knapsack-branch-and-bound
+    """
     pass
 
 def solve_it(input_data, capacity):
@@ -97,8 +133,9 @@ def solve_it(input_data, capacity):
         # Item(index, size, value)
         items.append(Item(int(parts[0]), int(parts[1]), int(parts[2])))
 
+    answer = knapsackExhaustive(items, capacity)
     # answer = knapsackGreedy(items, capacity)
-    answer = knapsackDP(items, capacity)
+    # answer = knapsackDP(items, capacity)
     return answer
 
 if __name__ == '__main__':
@@ -111,4 +148,3 @@ if __name__ == '__main__':
         print solve_it(input_data, capacity)
     else:
         print 'Usage: solver.py (file) (capacity)'
-
