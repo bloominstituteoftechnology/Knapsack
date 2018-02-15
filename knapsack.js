9ed7461d193a6d91e61773e30f0d8b396aa866d0
Dan Volosnikov <danvolosnikov@gmail.com>
2018-02-15 18:00:00 -0500

    exhaustive py to js

--- knapsack.js
+++ knapsack.js
@@ -10,8 +10,7 @@ const filename = args[0];
 const contents = fs.readFileSync(filename, 'utf8');
 
 const total = +args[1].split('=')[1];
-const matrix = contents
-  .split(/\r?\n/)
+const matrix = contents.split(/\r?\n/)
   .map((line) => line? line.split(' ').map((val) => +val) : null);
 matrix.pop();
 
@@ -21,17 +20,34 @@ matrix.pop();
 ////////////////////////
 // matrix.sort((a, b) => a[1]/a[2] - b[1]/b[2]);
 // const itemsToSelect = [];
-// let capacity = total, totalValue = 0, weight = 0, i = 0;
-
-// while (i < matrix.length) {
+// let capacity = total, totalValue = 0, weight = 0;
+// for (let i = 0; i < matrix.length; i++) {
 //   if (weight + matrix[i][1] <= capacity) {
 //     totalValue += matrix[i][2];
 //     itemsToSelect.push(matrix[i][0]);
 //     weight += matrix[i][1];
 //   }
-//   i += 1;
 // }
-
 // console.log('Items to select:', itemsToSelect.sort((a, b) => a - b));
-// console.log('Total cost:', weight);
-// console.log('Total value:', totalValue);
+// console.log('Total cost:', weight, '\nTotal value:', totalValue);
+
+///////////////////////////
+/// EXHAUSTIVE         ///
+///                   ///
+////////////////////////
+
+// const exhaustive = (items, capacity, value) => {
+//   if (!items.length || (items.length === 1 && items[0][1] > capacity)) {
+//     return value;
+//   } else if (items.length === 1) {
+//     value += items[0][2];
+//     return value;
+//   } else if (capacity >= items[0][1]) {
+//     return Math.max(exhaustive(items.slice(1), capacity - items[0][1],
+//       value + items[0][2]), exhaustive(items.slice(1), capacity, value))
+//   } else {
+//     return exhaustive(items.slice(1), capacity, value);
+//   }
+// };
+
+// console.log(exhaustive(matrix, total, 0));
