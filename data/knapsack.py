68618498ad335737ff576697a41bd896d71ff447
Christy Crites <cacrites2007@gmail.com>
2018-02-14 21:12:48 -0500

    its alive!

--- data/knapsack.py
+++ data/knapsack.py
@@ -1,34 +1,20 @@
 # index, weight, value
-# with open("small1.txt", "r") as data:
-#     lines = data.readlines()
-#     database =  # what on earth do I put here? lol
-#     for line in lines:
-#         s = line.rstrip().split()
-#         item = (int(s[0]), int(s[1]), int(s[2]))
-#         database.append(item)  # what do I put here?
-#     print("database", database)
+max_weight = 100
+cache = {}
 
-# if I remove the comments for the database here, it works.  Importing the file gives me
-# AttributeError: 'tuple' object has no attribute 'append'.
-# So the task at hand, is editing my file import to output a match to this data:
-# # database = (
-#     (1, 42, 81), (2, 42, 42), (3, 68, 56), (4, 68, 25),
-#     (5, 77, 14), (6, 57, 63), (7, 17, 75), (8, 19, 41),
-#     (9, 94, 19), (10, 34, 12),
-# )
-# At the moment, it looks like this:
-# database = [
-#     (1, 42, 81), (2, 42, 42), (3, 68, 56), (4, 68, 25),
-#     (5, 77, 14), (6, 57, 63), (7, 17, 75), (8, 19, 41),
-#     (9, 94, 19), (10, 34, 12),
-# ]
+with open("small1.txt", "r") as data:
+    lines = data.readlines()
+    database_list = []
+    for line in lines:
+        s = line.rstrip().split()
+        item = (int(s[0]), int(s[1]), int(s[2]))
+        database_list.append(item)
+    database = tuple(database_list)
+    print("database", database)
 
 
 def total_value(database, max_weight):
     return sum([x[2] for x in database]) if sum([x[1] for x in database]) < max_weight else 0
-
-
-cache = {}
 
 
 def solve(database, max_weight):
@@ -37,17 +23,15 @@ def solve(database, max_weight):
     if (database, max_weight) not in cache:
         head = database[0]
         tail = database[1:]
-        include = (head,) + solve(tail, max_weight - head[1])
-        dont_include = solve(tail, max_weight)
-        if total_value(include, max_weight) > total_value(dont_include, max_weight):
-            answer = include
+        compare_a = (head,) + solve(tail, max_weight - head[1])
+        compare_b = solve(tail, max_weight)
+        if total_value(compare_a, max_weight) > total_value(compare_b, max_weight):
+            result = compare_a
         else:
-            answer = dont_include
-        cache[(database, max_weight)] = answer
+            result = compare_b
+        cache[(database, max_weight)] = result
     return cache[(database, max_weight)]
 
-
-max_weight = 100
 
 solution = solve(database, max_weight)
 print("solution:", solution)
