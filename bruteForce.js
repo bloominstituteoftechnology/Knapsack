ebc87b4d950be13bfdab55378acb266e403462b8
CosmonautJones <travisj_jones@hotmail.com>
2018-02-15 18:11:13 -0600

    commiting work so far

--- bruteForce.js
+++ bruteForce.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-fs.readFile('data/small1.txt', (err, data) => {
+fs.readFile('data/small3.txt', (err, data) => {
   if (err) {
     console.error('Could not find file')
     process.exit(1)
@@ -18,15 +18,19 @@ fs.readFile('data/small1.txt', (err, data) => {
     }, [])
 
     console.log(dataArray);
+    const max = 100;
+    let array = [];
     let weight = 0;
     let value = 0;
+    let cost = 0;
+    while (weight < max) { 
         for (i = 0; i < dataArray.length; i++) {
-            for (j = i+1; j < dataArray.length; j++) {
-                for (k = j+1; k < dataArray.length; k++) {
+            for (j = i + 1; j < dataArray.length; j++) {
+                for (k = j + 1; k < dataArray.length; k++) {
                 weight = dataArray[i].size+dataArray[j].size+dataArray[k].size;
                 if (weight <= 100) {
-                    if ((dataArray[i].val+dataArray[j].val+dataArray[k].val) > value) {
-                        value = dataArray[i].val+dataArray[j].val+dataArray[k].val;
+                    if ((dataArray[i].val + dataArray[j].val + dataArray[k].val) > value) {
+                        value = dataArray[i].val + dataArray[j].val + dataArray[k].val;
                         console.log(`Items to select: ${dataArray[i].item}, ${dataArray[j].item}, ${dataArray[k].item}`);
                         console.log(`Total cost: ${weight}`);
                         console.log(`Total value: ${value}`);
@@ -34,5 +38,7 @@ fs.readFile('data/small1.txt', (err, data) => {
                 }
             }
         }
+    }
+    return;
     }
 })
