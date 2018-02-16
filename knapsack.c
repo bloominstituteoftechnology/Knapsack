449f8e90837fec8bb0d4c5536c76004bd821f281
owner <owner@Owners-MacBook-Air.local>
2018-02-15 17:20:43 -0800

    solution not quite working

--- knapsack.c
+++ knapsack.c
@@ -1,13 +1,10 @@
 #include <stdio.h> // contains printf
-//#include <stdlib.h> // contains FILE
-int max(int a, int b) { 
-    return (a > b)? a : b; 
-}
-    
+#include <stdlib.h> // contains FILE
+
 int main(void) {
 
     FILE* file;
-    file = fopen("./data/small2.txt", "r"); // 'r' is for reading
+    file = fopen("./data/small3.txt", "r"); // 'r' is for reading
     int index, weight, value;
     int arrIndex[11], arrWeight[11], arrValue[11];
     int i = 1;
@@ -26,26 +23,42 @@ int main(void) {
     int myWeight;
     int currentVal;
     int highestVal;
-    int savedIndex[11];
-    int weightMax = 100;
+    int weightMax;
     int n = sizeof(arrValue)/sizeof(arrValue[0]);
-    int knap[n+1][weightMax + 1]; 
-    for (int i = 0; i <= n; i++) {
-       for (int j = 0; j <= weightMax; j++) {
-            if (i==0 || j==0) {
-                knap[i][j] = 0;
-            } else if (arrWeight[i-1] <= j) {
-               knap[i][j] = max(arrValue[i-1] + knap[i-1][j - arrWeight[i-1]],  knap[i-1][j]);
-            }  else {
-                knap[i][j] = knap[i-1][j];
-            }      
+    char* concatinatedInt = "";
+    char* storeIndex;
+    char str[100];
+    char* indexUsed;
+    char* storeMaxIndex;
+    for (int b = 1; b < n; b++) {
+        // bug where indexUsed is somehow being changed outside the inner if statement
+        printf("%s\n", indexUsed);
+        for (int j = 1; j < n; j++) {
+            if (j == b) {
+                continue;
+            }
+            if ((myWeight + arrWeight[j] >= 100) && currentVal > highestVal) {
+                // bug where indexUsed should only be changed in this inner if statement
+                // but seem to changing outside of it. 
+                highestVal = currentVal;
+                weightMax = myWeight;
+                indexUsed = concatinatedInt;
+                printf("%s\n", indexUsed);
+            }
+            if (myWeight + arrWeight[j] >= 100) {    // printf("%s\n", indexUsed);
+                currentVal = 0;
+                myWeight = 0;
+                concatinatedInt = "";
+            }
+            currentVal += arrValue[j];
+            myWeight += arrWeight[j];
+
+            sprintf(str, "%s %d", concatinatedInt, arrIndex[j]);
+            int result=strtol(str, NULL, 10);
+            concatinatedInt = str;
         }
     }
-
-   printf("%d\n", knap[n][weightMax]);
-   // printf("%d, %d\n", myweight, myvalue);
-
-
+    printf("%s, %d, %d\n", indexUsed, weightMax, highestVal);
     fclose(file);
     return 0;
 }
