510a32fff16d675e118cdf1f73e715e1a70e36e8
JasonCorp84 <csababalogh84@gmail.com>
2018-03-22 14:00:47 -0700

    naiv recursion

--- src/knapsack.c
+++ src/knapsack.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
-#define MAX_SIZE 100
+#define MAX_SIZE 1000
 // A utility function that returns maximum of two integers
 int max(int a, int b) { return (a > b)? a : b; }
  
 // Returns the maximum value that can be put in a knapsack of capacity W
-int knapSack(int W, int wt[], int val[], int n)
+int knapSack(int maxW, int wt[], int val[], int itemNum)
 {
+    printf("Called knapsack, n = %d\n", itemNum);
    // Base Case
-   if (n == 0 || W == 0)
+   if (itemNum == 0 || maxW == 0)
        return 0;
  
    // If weight of the nth item is more than Knapsack capacity W, then
    // this item cannot be included in the optimal solution
-   if (wt[n-1] > W)
-       return knapSack(W, wt, val, n-1);
+   if (wt[itemNum-1] > maxW)
+       return knapSack(maxW, wt, val, itemNum-1);
  
    // Return the maximum of two cases: 
    // (1) nth item included 
    // (2) not included
-   else return max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1),
-                    knapSack(W, wt, val, n-1)
+   else return max( val[itemNum-1] + knapSack(maxW-wt[itemNum-1], wt, val, itemNum-1),
+                    knapSack(maxW, wt, val, itemNum-1)
                   );
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     //TODO: read command line argument to get file and size
 
@@ -65,6 +66,7 @@ int main (int argc, char *argv[])
         }
             printf("trying to get a number : %d\n", weights[3]);
             printf("we counted %d objects\n", numObjects);
+    }
         
         fclose(fp);
 
@@ -72,5 +74,5 @@ int main (int argc, char *argv[])
         
 
         printf("The answer is %d\n", result);
-    }
+    return 0;
 };
