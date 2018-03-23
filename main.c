fd11f76e1f45639561ab6353e50cbd50f461613e
Jesse Hood <jessenoob@gmail.com>
2018-03-23 08:32:49 -0400

    implemented a 'brute force' solution (and added the recursion solution from the lectures)

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// globals
 typedef struct Item {
     int id, weight, value;
 } Item;
@@ -9,8 +10,6 @@ typedef struct Item {
 Item items[1000];
 int length = 0;
 int weight_threshold = 300;
-
-
 
 
 FILE *get_file_ptr(char *filename)
@@ -52,7 +51,7 @@ void solution_one()
     int total_value = 0;
 
     Item selected_items[1000];
-    int selected_items_length;
+    int selected_items_length = 0;
     for (int i = 0; i < length; i++)
     {
         if (total_weight > weight_threshold) break;
@@ -65,10 +64,64 @@ void solution_one()
     print_results(selected_items, selected_items_length, total_weight, total_value);
 }
 
+// Brute force solution (it's definitely incomplete)
+void solution_two()
+{
+    int max_weight = 0;
+    int max_value  = 0;
+    Item *max_item_list;
+    int max_item_list_length = 0;
+    for (int i = 0; i < length; i++)
+    {
+        int total_weight = items[i].weight;
+        int total_value = items[i].value;
+
+        Item selected_items[1000];
+        int selected_items_length = 0;
+        selected_items[selected_items_length++] = items[i];
+
+        for (int j = 0; j < length; j++)
+        {
+            if (i == j) continue;
+            if (total_weight + items[j].weight > weight_threshold) break;
+            total_weight += items[j].weight;
+            total_value += items[i].value;
+            selected_items[selected_items_length++] = items[j];
+        }
+
+        if(total_value > max_value)
+        {
+            max_weight = total_weight;
+            max_value = total_value;
+            max_item_list = selected_items;
+            max_item_list_length = selected_items_length;
+        }
+    }
+
+    print_results(max_item_list, max_item_list_length, max_weight, max_value);
+}
+
+// Solution lecture / recursion
+// Didn't have the time to refactor this to print out in the same format that the other two solutions do
+int max(int a, int b) { return a > b ? a : b; }
+int solution_three(int itemNum)
+{
+    if (itemNum == 0 || weight_threshold == 0) return 0;
+
+    if (items[itemNum - 1].weight > weight_threshold) return solution_three(itemNum - 1);
+
+    else return max(
+        items[itemNum - 1].value + solution_three(itemNum - 1),
+        solution_three(itemNum - 1)
+    );
+}
+
+
 int main(int argc, char *argv[])
 {
     int id, weight, value;
 
+    // build an array of items
     FILE *file = get_file_ptr(argv[1]);
     int i = 0;
     while(fscanf(file, "%d %d %d\n", &id, &weight, &value) != EOF)
@@ -81,6 +134,12 @@ int main(int argc, char *argv[])
         i++;
     }
     length = i;
+    fclose(file);
+
+    // uncomment a solution to run it (comment all others)
     solution_one();
+    // solution_two();
+    // printf("best value: %d\n", solution_three(length));
+
     return 0;
 }
