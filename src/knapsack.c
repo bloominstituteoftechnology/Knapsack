145df8782e91bd7e988beab1db927bb6d447a583
Adam Fetters <adamfetterscoder@gmail.com>
2018-03-23 08:21:57 -0400

    Did 3 naive solutions

--- src/knapsack.c
+++ src/knapsack.c
@@ -2,6 +2,10 @@
 
 #define MAX_SIZE 1000
 
+void loadFile()
+{
+}
+
 int main(int argc, char *argv[])
 {
   //TODO: read command line argument to get file and size
@@ -66,10 +70,11 @@ int main(int argc, char *argv[])
 
   // Adds up by highest value so that corresponding weight is less than or equal to 100
 
-  int totalWeight = 0;
+  int totalWeight = 0, totalValue = 0;
   int itemsSelected[10] = {0};
   int numItems = 0;
   printf("Highest value less than or equal to weight 100, added by highest values first, then 2nd, etc..\n");
+  printf("--------------------------------------------------\n");
   for (int i = 0; i < 10; i++)
   {
     totalWeight += weights[i];
@@ -78,13 +83,15 @@ int main(int argc, char *argv[])
     {
       itemsSelected[numItems] = i;
       printf("item %d with weight of %d: value %d:\n", i, weights[i], values[i]);
+      totalValue += values[i];
     }
     else
     {
       totalWeight -= weights[i];
     }
   }
-  printf("For a total weight of %d\n\n", totalWeight);
+  printf("For a total weight of %d and a total value of %d\n\n", totalWeight, totalValue);
+  printf("Highest value less than or equal to weight 100, added by lowest weight first, then second, etc...\n");
   printf("--------------------------------------------------\n");
 
   // Sorts objects in ascending order of weight
@@ -106,23 +113,75 @@ int main(int argc, char *argv[])
     }
   }
 
-  totalWeight = 0;
+  totalWeight = 0, totalValue = 0;
   int itemsSelectedByWeight[10] = {0};
   numItems = 0;
-  printf("Highest value less than or equal to weight 100, added by lowest weight first, then second, etc...\n");
+
   for (int i = 0; i < 10; i++)
   {
     totalWeight += weights[i];
 
     if (totalWeight <= maxWeight)
     {
-      itemsSelected[numItems] = i;
+      itemsSelectedByWeight[numItems] = i;
       printf("item %d with weight of %d: value %d:\n", i, weights[i], values[i]);
+      totalValue += values[i];
     }
     else
     {
       totalWeight -= weights[i];
     }
   }
-  printf("For a total weight of %d\n", totalWeight);
+
+  // Sorts objects in ascending order of weight/value ratio
+
+  temp;
+  double ratio1 = 0.0, ratio2 = 0.0;
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = i + 1; j < 10; j++)
+    {
+      ratio1 = (double)weights[i] / values[i];
+      ratio2 = (double)weights[j] / values[j];
+
+      if (ratio1 < ratio2)
+      {
+
+        temp = weights[j];
+        weights[j] = weights[i];
+        weights[i] = temp;
+
+        temp = values[j];
+        values[j] = values[i];
+        values[i] = temp;
+      }
+    }
+  }
+  printf("For a total weight of %d and a total value of %d\n\n", totalWeight, totalValue);
+  printf("Highest value less than or equal to weight 100, added by ratio of value to weight ratio\n");
+
+  printf("--------------------------------------------------\n");
+
+  // Adds items based on new desending ratio list
+  totalWeight = 0, totalValue = 0;
+  ;
+  int itemsSelectedByRatio[10] = {0};
+  numItems = 0;
+
+  for (int i = 0; i < 10; i++)
+  {
+    totalWeight += weights[i];
+
+    if (totalWeight <= maxWeight)
+    {
+      itemsSelectedByRatio[numItems] = i;
+      printf("item %d with weight of %d: value %d:\n", i, weights[i], values[i]);
+      totalValue += values[i];
+    }
+    else
+    {
+      totalWeight -= weights[i];
+    }
+  }
+  printf("For a total weight of %d and a total value of %d\n", totalWeight, totalValue);
 };
