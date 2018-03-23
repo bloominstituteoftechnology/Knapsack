2cc85ae2afcbe13d93873133334cc226ff1cf41b
JASPINDER SING <jaspindersingh83@gmail.com>
2018-03-22 23:47:43 -0600

    Added solutions with C as well

--- solution.js
+++ solution.js
@@ -8,7 +8,7 @@ let vals = [];
 
 // Read file and define threshhold
 let capacity = 100;
-let content = fs.readFileSync('./data/medium2.txt','utf-8');
+let content = fs.readFileSync('./data/small2.txt','utf-8');
 let inputArr = content.split('\n')
 inputArr.pop();
 console.log(inputArr)
@@ -22,28 +22,42 @@ for(let i = 0; i < inputArr.length; i++){
     wts.push(parseInt(temp[1]))
     vals.push(parseInt(temp[2]))
 }
-
+console.log(wts)
 //Method 1 with recursion
 
-function knapsack(capacity,i){  
-    // console.log(selected)
+function knapsack(anyarray,capacity,n){ 
     //Base Case
-    if(capacity === 0 || i >= n-1) {
+    if(capacity === 0 || n===0) {
         return 0;
     }
     //If nth items has a weight > capacity
-    if(wts[i] > capacity){
-        return knapsack(capacity,i+1)
+    if(wts[n-1] > capacity){
+        return knapsack(anyarray,capacity,n-1)
     }
-
     //Take the max of 2 case
     //1) If item is selected
     //2) if items is not selected; 
-    let case1 = knapsack(capacity-wts[i],i+1)+vals[i];
-    let case2 = knapsack(capacity,i+1)
-    return(Math.max(case1,case2))
+    let max;
+    let v1 = anyarray.slice(0);
+    v1[n-1] =1;
+    let case1 = knapsack(v1,capacity-wts[n-1],n-1)+vals[n-1];
+    let case2 = knapsack(anyarray,capacity,n-1)
+    if(case1 > case2){
+        console.log('from here');
+        anyarray=v1.slice(0);
+        max =case1;
+    } else {
+        max = case2;
+    }
+    console.log(anyarray)
+    return(max)
 }
-// console.log(knapsack(capacity,0))
+let selectedArr = [];
+for(let i =0; i <inputArr.length; i++){
+    selectedArr.push(0)
+}
+console.log(knapsack(selectedArr,capacity,n))
+console.log(selectedArr)
 
 
 //Method 2 ...with Dynamic programming
@@ -56,11 +70,8 @@ for(let i =0; i<= capacity+1; i++){
     }
 }
 
-
-function dynamicKs(capacity, i){
-   
+function dynamicKs(capacity, i){ 
     if(i >= n-1 || capacity === 0){
-        // console.log(visited[])
         visited[capacity][i] = 0;
         return 0;
     }
@@ -70,6 +81,7 @@ function dynamicKs(capacity, i){
         }
         return visited[capacity][i+1]
     }
+    //Case1 Items Seleted //Case2 Not selected
     let case1; let case2;
     if(!visited[capacity-wts[i]][i+1]){
         visited[capacity-wts[i]][i+1] = dynamicKs(capacity-wts[i],i+1);
