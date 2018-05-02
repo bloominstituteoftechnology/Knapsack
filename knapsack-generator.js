3ea775711de76a01ff34f08393ca036579867885
Punit Rawal <rawalhome@gmail.com>
2018-05-02 15:11:22 -0700

    Steve Magadan + Punit Rawal: reading lines in

--- knapsack-generator.js
+++ knapsack-generator.js
@@ -1,41 +1,55 @@
 //This file generates more data sets for the knapsack solver to process.
 //It is not a part of any solution to this problem.
+// This is punit typing.....he's a good typist..
 
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 const args = process.argv.slice(2);
+let fileName = args[0];
 
-if (args.length != 2) {
-  console.error("usage: knapsack-generator filename size");
-  process.exit(2);
-}
-
-let [filename, size] = args;
-const filePath = path.resolve(__dirname, filename);
-
-if (fs.existsSync(filePath)) {
-  console.error(`File "${filePath}" already exists!`);
-  process.exit(2);
-}
-
-if (isNaN(size)) {
-  console.error(`size must be a number!`);
-  process.exit(2);
-}
-
-size = ~~Number(size);
-
-let text = '';
-
-for (let i = 1; i <= size; i++)
-  text += `${i} ${Math.floor(Math.random() * 101)} ${Math.floor(Math.random() * 101)}${i === size ? '' : '\n'}`;
-
-fs.writeFile(filePath, text, (error) => {
-    if(error) {
-      console.error('The file could not be saved');
-      process.exit(1);
-    }
-
-    console.log(`Knapsack problem saved to ${filePath}`);
+let rl = readline.createInterface({
+  input: fs.createReadStream(args[0]),
 });
+
+rl.on('line', (line) => {
+  console.log(line);
+});
+
+// rl.close();
+
+
+// if (args.length != 2) {
+//   console.error("usage: knapsack-generator filename size");
+//   process.exit(2);
+// }
+
+// let [filename, size] = args;
+// const filePath = path.resolve(__dirname, filename);
+
+// if (fs.existsSync(filePath)) {
+//   console.error(`File "${filePath}" already exists!`);
+//   process.exit(2);
+// }
+
+// if (isNaN(size)) {
+//   console.error(`size must be a number!`);
+//   process.exit(2);
+// }
+
+// size = ~~Number(size);
+
+// let text = '';
+
+// for (let i = 1; i <= size; i++)
+//   text += `${i} ${Math.floor(Math.random() * 101)} ${Math.floor(Math.random() * 101)}${i === size ? '' : '\n'}`;
+
+// fs.writeFile(filePath, text, (error) => {
+//     if(error) {
+//       console.error('The file could not be saved');
+//       process.exit(1);
+//     }
+
+//     console.log(`Knapsack problem saved to ${filePath}`);
+// });
