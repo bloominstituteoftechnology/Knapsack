94d334e19c8f9e7bd142a07339de37d931e4dde9
Dani Tacheny <dani.tacheny@gmail.com>
2018-05-02 18:25:05 -0500

    sort by ratio of value/cost

--- solver.js
+++ solver.js
@@ -14,3 +14,13 @@ let data = fs.readFileSync(filename, 'utf8');
 
 data = data.trim().split('\n').map(x => x.split(' '));
 data = data.map(x => [Number(x[0]), Number(x[1]), Number(x[2])]);
+
+for (let i = 0; i < data.length; i++) {
+  data[i].push(data[i][2] / data[i][1]);
+}
+
+data = data.sort((a, b) => {
+  if (a[3] > b[3]) return -1;
+  else if (a[3] < b[3]) return 1;
+  else return 0;
+});
