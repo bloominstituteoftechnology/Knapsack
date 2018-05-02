53608b455e76cfde538b6fb346080f74caa58863
Cliff Kang <inlovewithhim@gmail.com>
2018-05-02 16:47:45 -0700

    added Ronnie's solution to study later

--- Knapsack.js
+++ Knapsack.js
@@ -125,3 +125,59 @@ console.log(
 
 // // const value = knapsackRecursive(items, capacity);
 // // console.log(value);
+
+
+// // Ronnie's code
+// const fs = require('fs');
+
+// const loadFile = () => {
+//   if(process.argv[2] && process.argv[3]) {
+//     return fs.readFileSync(process.argv[2], 'utf-8');
+//   } else {
+//     console.error('Please supply a file to read from and a knapsack capacity');
+//     process.exit(0);
+//   }
+// };
+
+// const Main = () => {
+//   const table = [];
+//   const values = [];
+//   const weights = [];
+//   const file = loadFile();
+//   const capacity = process.argv[3];
+
+//   let arr = file.trim().split('\n');
+//   const n = arr.length;
+//   for(let e = 0; e < n; e++) {
+//     out = arr[e].split( ' ' );
+//     values.push(+out[2]);
+//     weights.push(+out[1]);
+//   }
+
+//   for(let i = 0; i < n; i++) {
+//     table[i] = [];
+//   }
+
+//   for(let j = 0; j < capacity; j++) {
+//     table[0][j] = 0;
+//   }
+
+//   for(let i = 1; i < n; i++) {
+//     for(let j = 0; j < capacity; j++) {
+//       if(weights[i] > j) {
+//         table[i][j] = table[i - 1][j];
+//       } else {
+//         table[i][j] = Math.max(table[i - 1][j], table[i - 1][j - weights[i]] + values[i]);
+//       }
+//     }
+//   }
+
+//   // console.log('arr: ', arr);
+//   console.log('val: ', values);
+//   console.log('wgt: ', weights);
+//   console.log('TABLE: ', table);
+//   // console.log(((end - start) / 1000).toFixed(4));
+//   console.log('OPTIMAL: ', table[n - 1][capacity - 1]);
+// };
+
+// Main();
