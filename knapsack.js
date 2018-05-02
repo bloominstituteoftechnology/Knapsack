4904293784f259543d99369a569b106bcc2e876d
Mephestys <mephestys@gmail.com>
2018-05-02 14:37:38 -0600

    `knapsack.js` separates `weight` and `values` from file into two arrays

--- knapsack.js
+++ knapsack.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 const args = process.argv.slice(2);
+const weights = [], values = [];
 
 if (args.length != 2) {
   console.error("usage: knapsack filename sack-size");
@@ -14,8 +15,17 @@ if (!Number.isInteger(sackSize)) {
   process.exit(2);
 }
 
-const file = fs.readFileSync(`./data/${args[0]}`, 'utf8', (err, fd) => {
+const treasure = fs.readFileSync(`./data/${args[0]}`, 'utf8', (err, fd) => {
   if (err) throw err;
-});//.split('\n');
+}).split('\n');
 
-console.log(file);
+for (let item of treasure) {
+  item = item.trim();
+  if (item === '') continue;
+  item = item.split(' ');
+  weights.push(parseInt(item[1], 10));
+  values.push(parseInt(item[2], 10));
+}
+
+console.log(`${sackSize} slots total in knapsack.`);
+console.log(weights);
