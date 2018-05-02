fbb3fe05832269a89f011b49254812472c802473
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-05-02 17:58:48 -0400

    Some sort of solution here...not sure if correct
    - Put all of the item info into a new array along with their Value/Weight ratios
    - Sort the array with best ratios at the top
    - For as long as current weight is less than threshold, add the item and keep
    track of it
    - Print out results

--- knapsack.js
+++ knapsack.js
@@ -18,28 +18,91 @@ items.pop();
 for(let i = 0; i < items.length; i++) {
   items[i] = items[i].split(' ');
 }
-// Input:
-// Values (stored in array v) 3rd number
-let values = [];
+
+let ratios = [];
+
 items.forEach(item => {
-  values.push(parseInt(item[2]));
+  let temp = [];
+  temp.push(item[0]);
+  temp.push(parseInt(item[2]/item[1]));
+  temp.push(parseInt(item[1]));
+  temp.push(parseInt(item[2]));
+  ratios.push(temp);
 })
 
-// Weights (stored in array w) 2nd number
-let weights = [];
-items.forEach(item => {
-  weights.push(parseInt(item[1]));
-})
-// Number of distinct items (n)
-let n = items.length;
+ratios.sort(sortFunction);
 
-// Knapsack capacity (W)
-threshold = parseInt(threshold);
-
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    if (weights[i] > j) {
-
-    }
+function sortFunction(a, b) {
+  if (a[0] === b[0]) {
+    return 0;
   }
+  return (a[1] < b[1]) ? 1 : -1;
 }
+
+let result = {};
+let currentWeight = 0;
+let finalWeight = 0;
+let finalValue = 0;
+
+for (let i = 0; i < ratios.length; i++) {
+  if (currentWeight + (ratios[i][2]) <= threshold) {
+    currentWeight += (ratios[i][2]);
+    // result.push(ratios[i]);
+    result[ratios[i][0]] = {};
+    result[ratios[i][0]]['weight'] = (ratios[i][2]);
+    result[ratios[i][0]]['value'] = (ratios[i][3]);
+    finalWeight += (ratios[i][2]);
+    finalValue += (ratios[i][3]);
+  }
+};
+
+
+
+console.log(`Items to select: ${Object.keys(result)}`);
+console.log(`Total Weight: ${finalWeight}`);
+console.log(`Total Value: ${finalValue}`);
+
+// // Input:
+// // Values (stored in array v) 3rd number
+// let values = [];
+// items.forEach(item => {
+//   values.push(parseInt(item[2]));
+// })
+
+// // Weights (stored in array w) 2nd number
+// let weights = [];
+// items.forEach(item => {
+//   weights.push(parseInt(item[1]));
+// })
+// // Number of distinct items (n)
+// let n = items.length;
+
+// // Knapsack capacity (W)
+// threshold = parseInt(threshold);
+
+
+// const knapsack = {};
+
+// if (args.length != 2) {
+//   console.error("require correct arguments");
+//   process.exit(1);
+// }
+
+// const filename = args[0];
+// const threshold = args[1];
+
+// let lineReader = require('readline').createInterface({
+//   input: fs.createReadStream(filename)
+// });
+
+// lineReader.on('line', (line) => {
+//   let splitLine = line.split(' ');
+//   let itemInfo = [];
+//   itemInfo.push(splitLine[1]);
+//   itemInfo.push(splitLine[2]);
+//   knapsack[splitLine[0]] = itemInfo;
+// });
+
+// lineReader.on('end', () => {
+//   lineReader.close()
+// });
