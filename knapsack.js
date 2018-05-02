09e29a5c56f62104d3c1152e0ad9ab2ab956a54f
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-05-02 17:04:26 -0400

    organized data into array of objects

--- knapsack.js
+++ knapsack.js
@@ -1,29 +1,41 @@
-const fs = require('fs');
-const path = require('path');
-const rl = require('readline');
+const fs = require("fs");
+const path = require("path");
+const rl = require("readline");
 
 const args = process.argv.slice(2);
 
 if (args.length != 2) {
-    console.error("usage: knapsack filename size");
-    process.exit(2);
+  console.error("usage: knapsack filename maxsize");
+  process.exit(2);
 }
 
-let [filename, size] = args;
+let [filename, maxsize] = args;
 const filePath = path.resolve(__dirname, filename);
 
-if (fs.existsSync(filePath)) {
-    console.log('File to process', filePath);
-    const lineReader = rl.createInterface({input: fs.createReadStream(filePath)});
-    lineReader.on('line', function (line) {
-        let item = line.split(' ')[0];
-        let size = line.split(' ')[1];
-        let value = line.split(' ')[2];
-        console.log('item:', item, ' size:', size, ' value:', value);
-    });
-}
+fs.readFile(filename, (err, data) => {
+  if (err) {
+    console.error("Couldn't find file");
+    process.exit(1);
+  }
 
-if (isNaN(size)) {
-    console.error(`size must be a number!`);
-    process.exit(2);
+  const inventory = data
+    .toString()
+    .split("\r\n")
+    .reduce((items, item, index) => {
+      if (item) {
+        const [id, cost, value] = item.split(" ");
+        const effeciency = Math.round(value / cost * 10) / 10;
+        items[index] = { id, cost, value, effeciency };
+      }
+      return items;
+    }, []);
+  inventory.sort((a, b) => {
+    return parseFloat(b.effeciency) - parseFloat(a.effeciency);
+  });
+  console.log(inventory);
+});
+
+if (isNaN(maxsize)) {
+  console.error(`Knapsack size must be a number!`);
+  process.exit(2);
 }
