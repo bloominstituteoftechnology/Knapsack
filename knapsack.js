b3e6f564a6652dda94bb806ef96e93ade26ef87d
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-03 10:08:26 -0500

    Greedy Approximation Algorithm
    - in knapsack.js:
    - refactor items assignment/destructuring (line 17...)
    - sort array by density (value / size)
    - unshift undefined and pop items, to "reposition" empty index
    - initialize/assign variables
    - loop over items
    - add item until capacity precludes
    - add/or increment item in final object holding inventory
    - break if capacity reached
    - test and console log results

--- knapsack.js
+++ knapsack.js
@@ -14,11 +14,28 @@ const items = [];
 
 for (let line of entries) {
   const [index, size, value] = line.split(/\s+/).map(n => +n);
-  items[index] = {
-    index,
-    size,
-    value,
-  };
+  items[index] = { index, size, value, density: value / size };
+}
+
+items.sort((a, b) => b.density - a.density);
+items.unshift(undefined);
+items.pop();
+
+let value = 0;
+let volume = 0;
+const final = {};
+
+for (let i = 1; i < items.length; i++) {
+  while (volume + items[i].size <= capacity) {
+    volume += items[i].size;
+    if (final[items[i].index]) {
+      final[items[i].index] += 1;
+    } else {
+      final[items[i].index] = 1;
+    }
+  }
+  if (volume === capacity) break;
 }
 
 console.log(items);
+console.log(final);
