96805d42551a27fd48e1c8fc37b0f8625f145108
german gonzalez <gcomlnk@gmail.com>
2018-05-03 01:20:21 -0400

    Pop the highest value from sorted results

--- knapsack.js
+++ knapsack.js
@@ -1,8 +1,5 @@
 const fs = require('fs');
 const args = process.argv.slice(2);
-const values = [];
-const sizes  = [];
-const table  = [];
 
 if(args.length != 2) {
     console.error("usage: ./knapsack fileData threshold");
@@ -22,9 +19,7 @@ const file = getData();
 let lines = file.trim().split('\n');
 
 const start = Date.now();
-const preProcess = [];
-const subProcess = [];
-const goodResults = [];
+const preProcess = []; const subProcess = []; const goodResults = [];
 
 for(let i = 0; i < lines.length; i++) {
     const line = lines[i].split(' ');
@@ -33,65 +28,56 @@ for(let i = 0; i < lines.length; i++) {
 }
 
 preProcess.map((x, i) => {
-
     let sizeResult = 0;
     let res = [];
-    console.log('Main:',preProcess[i]);
-
     subProcess.forEach(function(el, j) {
-
         if(x !== el){
-            // console.log('   SubProcess:', subProcess[j]);
-
             if(sizeResult < threshold && parseInt(preProcess[i][1]) < threshold){
-
                 sizeResult = parseInt(preProcess[i][1]) + parseInt(subProcess[j][1]) + sizeResult;
-
-                // console.log('   sizeResult',  sizeResult);
-
                 if(sizeResult <= threshold){
-
                     if(res.length === 0){
                         res.push(x);
                     }
-
                     res.push(el);
-
                 }
-
             }
-
             if(preProcess[i][1] === threshold && res.length === 0){
                 res.push(preProcess[i]);
             }
-
             if(res.length > 0){
-                console.log('res', res);
+                // console.log('res', res);
             }
         }
     });
 
-    goodResults.push(res)
-
-
+    goodResults.push(res);
 });
 
+// Sum the values of each good combination.
 goodResults.forEach(function(el, i) {
-
-    console.log('goodResult element', el);console.log('');
-
     let rsp = 0;
-
     el.forEach(item => { rsp = parseInt(item[2]) + rsp });
-
     goodResults[i].push(rsp);
-
-    console.log('rsp::', goodResults);
-
-
 });
+
+// Sorting the good combinations values results ASC
+const sortFunction = function(a, b) {
+    if (a[a.length-1] === b[b.length-1])
+        return 0;
+    else
+        return (a[a.length-1] < b[b.length-1]) ? -1 : 1;
+};
+
+goodResults.sort(sortFunction);
+
+// Pop the last record which should be the highest
+const itemToSelect =  goodResults.pop();
+
+console.log('Items to select:');
+console.log(itemToSelect);
+console.log('');
+console.log('Total value:', itemToSelect[itemToSelect.length-1]);
 
 const end = Date.now();
 
 console.log('EXCUTION TIME: ', ((end - start) / 1000).toFixed(4));
-
