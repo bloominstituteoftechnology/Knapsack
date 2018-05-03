fdb26ea7bc36dc642c705b17b865e077eaabf915
Mephestys <mephestys@gmail.com>
2018-05-02 19:00:09 -0600

    Recursive solution - Does not track which items were selected, or cost. - Added process timer.

--- knapsack.js
+++ knapsack.js
@@ -2,6 +2,23 @@ const fs = require("fs");
 
 const args = process.argv.slice(2);
 const weights = [], values = [];
+// let cost = 0;
+
+function knapSackRecursive(capacity) {
+  function ksr(i, size) {
+    if (i === 0) {
+      return 0;
+    } else if (weights[i] > size) {
+      return ksr(i - 1, size);
+    } else {
+      return Math.max(
+        ksr(i - 1, size),
+        ksr(i - 1, size - weights[i]) + values[i],
+      );
+    }
+  }
+  return ksr(weights.length - 1, capacity);
+}
 
 if (args.length < 1) {
   console.error("usage: knapsack <filename> [sack-size]");
@@ -16,7 +33,7 @@ if (!Number.isInteger(sackSize)) {
 
 const treasure = fs.readFileSync(`./data/${args[0]}`, 'utf8', (err, fd) => {
   if (err) throw err;
-}).split('\n');
+}).split(/\r?\n/);
 
 for (let item of treasure) {
   item = item.trim();
@@ -26,5 +43,9 @@ for (let item of treasure) {
   values.push(parseInt(item[2], 10));
 }
 
+console.time('Process Time');
+const value = knapSackRecursive(sackSize);
+
 console.log(`${sackSize} slots total in knapsack.`);
-console.log(weights);
+console.log(`Slots Used: TODO, Total Value: ${value}`);
+console.timeEnd('Process Time');
