9be6c8f35aaea40ee848088d881fc2439e361115
Mephestys <mephestys@gmail.com>
2018-05-02 19:27:35 -0600

    Better recursive solution (following solution lecture) - Now outputs chosen items, capacity used, and total value. - Originally only output total capacity, value, and processing time.

--- knapsack.js
+++ knapsack.js
@@ -1,23 +1,52 @@
 const fs = require("fs");
 
 const args = process.argv.slice(2);
-const weights = [], values = [];
+const items = [];
+// const weights = [], values = [];
 // let cost = 0;
 
-function knapSackRecursive(capacity) {
-  function ksr(i, size) {
+// function knapSackRecursive(capacity) {
+//   function ksr(i, size) {
+//     if (i === 0) {
+//       return 0;
+//     } else if (weights[i] > size) {
+//       return ksr(i - 1, size);
+//     } else {
+//       return Math.max(
+//         ksr(i - 1, size),
+//         ksr(i - 1, size - weights[i]) + values[i],
+//       );
+//     }
+//   }
+//   return ksr(weights.length - 1, capacity);
+// }
+
+function knapSackRecursive(items, capacity) {
+  function recur(i, size) {
     if (i === 0) {
-      return 0;
-    } else if (weights[i] > size) {
-      return ksr(i - 1, size);
+      return {
+        value: 0,
+        size: 0,
+        chosen: [],
+      };
+    } else if (items[i].size > size) {
+      return recur(i - 1, size);
     } else {
-      return Math.max(
-        ksr(i - 1, size),
-        ksr(i - 1, size - weights[i]) + values[i],
-      );
+      const r0 = recur(i - 1, size);
+      const r1 = recur(i - 1, size - items[i].size);
+
+      r1.value += items[i].value;
+
+      if (r0.value > r1.value) {
+        return r0;
+      } else {
+        r1.size += items[i].size;
+        r1.chosen = r1.chosen.concat(i);
+        return r1;
+      }
     }
   }
-  return ksr(weights.length - 1, capacity);
+  return recur(items.length - 1, capacity);
 }
 
 if (args.length < 1) {
@@ -38,14 +67,20 @@ const treasure = fs.readFileSync(`./data/${args[0]}`, 'utf8', (err, fd) => {
 for (let item of treasure) {
   item = item.trim();
   if (item === '') continue;
-  item = item.split(' ');
-  weights.push(parseInt(item[1], 10));
-  values.push(parseInt(item[2], 10));
+  const [index, size, value] = item.split(/\s+/).map(n => parseInt(n));
+  // weights.push(parseInt(item[1], 10));
+  // values.push(parseInt(item[2], 10));
+  items[index] = {
+    index: index,
+    size: size,
+    value: value,
+  };
 }
 
 console.time('Process Time');
-const value = knapSackRecursive(sackSize);
+const result = knapSackRecursive(items, sackSize);
 
 console.log(`${sackSize} slots total in knapsack.`);
-console.log(`Slots Used: TODO, Total Value: ${value}`);
+console.log(`Chosen: ${result.chosen}`);
+console.log(`Slots Used: ${result.size}, Total Value: ${result.value}`);
 console.timeEnd('Process Time');
