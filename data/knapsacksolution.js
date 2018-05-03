745c03a78b83e1305343e30853c3b99fa60e4fa1
Dixie Korley <dixiekorley@gmail.com>
2018-05-03 17:14:58 -0400

    Moved knapsacksolution to data

--- data/knapsacksolution.js
+++ data/knapsacksolution.js
@@ -23,3 +23,31 @@
 
 // console.log(items);
 
+const fs = require("fs");
+const args = process.argv.slice(2); // node is the first argument
+
+if (args.length != 2) {
+    console.error("Usage: Knapsack infile capacity");
+    process.exit(1);
+}
+
+const filename = args[0];
+const capacity = args[1];
+
+const filedata = fs.readFileSync(filename, "utf8");
+
+const lines = filedata.trim().split(/[\r\n]+/);
+
+const items = [];
+
+for (let i of lines) {
+    const [index, size, value] = l.split(/\s+/).map(n => parseInt(n));
+
+    items = {
+        index: index,
+        size: size,
+        value: value
+    };
+}
+
+console.log(items);
