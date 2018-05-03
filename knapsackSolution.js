8d91866685201de8901c78b45fb4328cd3ae31fd
Eric <erichech@gmail.com>
2018-05-03 17:17:44 -0400

    prettified

--- knapsackSolution.js
+++ knapsackSolution.js
@@ -16,19 +16,18 @@ let parsed = data
   .trim()
   .split('\n')
   .map(each => each.split(' '));
-parsed = parsed.map(each => [
-  Number(each[0]),
-  Number(each[1]),
-  Number(each[2]),
-]);
 
+let parsedNum = parsed.map(each => [+each[0], +each[1], +each[2]]);
+
+//
+// ────────────────────────────────────────────────────────── START THE TIMER ─────
 const startTime = Date.now();
 
-for (let i = 0; i < parsed.length; i++) {
-  parsed[i].push(parsed[i][2] / parsed[i][1]);
+for (let i = 0; i < parsedNum.length; i++) {
+  parsedNum[i].push(parsedNum[i][2] / parsedNum[i][1]);
 }
 
-parsed = parsed.sort((a, b) => {
+parsedNum = parsedNum.sort((a, b) => {
   if (a[3] > b[3]) return -1;
   else if (a[3] < b[3]) return 1;
   else return 0;
@@ -36,14 +35,17 @@ parsed = parsed.sort((a, b) => {
 
 let value = 0;
 let cost = 0;
-for (let i = 0; i < parsed.length; i++) {
-  if (cost + parsed[i][1] <= 100) {
-    cost += parsed[i][1];
-    value += parsed[i][2];
+for (let i = 0; i < parsedNum.length; i++) {
+  if (cost + parsedNum[i][1] <= 100) {
+    cost += parsedNum[i][1];
+    value += parsedNum[i][2];
   }
 }
 
 const endTime = Date.now();
+// ──────────────────────────────────────────────────────────── END THE TIMER ─────
+//
+
 
 console.log('THRESHOLD:', threshold);
 console.log('Value:', value);
