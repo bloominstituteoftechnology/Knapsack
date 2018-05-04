69e7d7109d83886e31c0400d37a332bbb75c1713
shobanavv <shobanavv@gmail.com>
2018-05-03 22:01:20 -0700

    Tried knapsack recursion

--- knapsack.js
+++ knapsack.js
@@ -1,5 +1,29 @@
 const fs = require('fs');
 
+
+
+
+
+function knapsackRecursive(items, thresold) {
+    function recur(i, size) {
+        
+        //base case 
+        if(i == 0 || size == 0) {
+            return 0;
+        } 
+        else if(items[i] > size) {
+            return recur(i-1, size);
+        }
+        else {
+            return Math.max(
+                recur(i-1, size),
+                recur(i-1, size - items[i]) + items[i]
+            );
+        }
+    }
+    return recur(items.length -1, thresold);
+}
+
 const args = process.argv.slice(2);
 
 const knapsack = {};
@@ -15,4 +39,8 @@ let itemInfo = fs.readFileSync(filename,"utf8");
 
 let items = itemInfo.split('\n');
 items.pop();
-console.log(items);
+//console.log(items);
+
+
+const value = knapsackRecursive(items, thresold);
+console.log(value);
