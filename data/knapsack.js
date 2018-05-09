4aa79387240e879e8e9f77b917d70237f20fc919
William Pelton <williamrpelton@gmail.com>
2018-05-09 16:48:37 -0500

    William Pelton Charletta Bullard complete

---
+++ data/knapsack.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+function naiveKnapsack(items, capacity) {
+  items.sort((a,b) => {
+    return (a.size/a.value) - (b.size/b.value);
+  });
+  
+  let chosen = [];
+  let totalSize = 0;
+  let totalValue = 0;
+
+  items.forEach(item => {
+    if (item.size < capacity - totalSize) {
+      chosen.push(item);
+      totalSize += item.size;
+      totalValue += item.value;
+    } else return;
+  }); return (chosen);
+}
+
+const argv = process.argv.slice(2);
+
+// add an error check to check the number of params
+if (argv.length != 2) {
+  console.error('usage: [filename] [capacity]');
+  process.exit(1);
+}
+
+const filename = argv[0];
+const capacity = argv[1];
+
+// read the file that was passed to our program
+
+const filedata = fs.readFileSync(filename, 'utf8');
+
+const lines = filedata.trim().split(/[\r\n]+/g);
+
+// process the lines 
+
+const items = [];
+
+for (let l of lines) {
+  const [index, size, value] = l.split(' ').map(n => parseInt(n));
+  
+  items[index] = {
+    index,
+    size,
+    value,
+  };
+}
+
+console.log("Real good implementation: ", naiveKnapsack(items, capacity));
