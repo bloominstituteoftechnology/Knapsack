95e9198777d2636847aab2c45c901934e421ac22
Brett Sullivan <opticalfalicy@gmail.com>
2018-05-10 16:21:55 -0500

    Progress

--- myKnapsack.js
+++ myKnapsack.js
@@ -1,113 +1,48 @@
 const fs = require('fs');
 const firstBy = require('thenby');
 
-function loopKnapsack(items, capacity) {
+function loopSack(items, capacity) {
 
-    // items.sort(function (b, a) {
-    //     // return a.value - b.value;
-    //     // b.size - a.size;
-    //     // return(b, a);
 
-    //     // if(a.size > b.size) return -1;
-    //     // if(a.size < b.size) return 1;
 
-    //     // if(a.value > b.value) return 1;
-    //     // if (a.value < b.value) return -1;
-
-    //     return a.value - b.value && b.size + a.size;
-
-    // })
-
-    // for(let i = 1; i < items.length; i++){
-    //     let sorter;
-    //     if (items[i].value > items.value){
-    //       return sorter = items[i].length++
-    //     }
-        // if(items[i].size < items.size){
-        //     items[i].size.length++
-        // }
-    
         items.sort(function(item1, item2){
 
             let item1ValSize = item1.value / item1.size;
             let item2ValSize = item2.value / item2.size;
             return item1ValSize < item2ValSize ? 1 : -1;
-        }
-
-        //     firstBy(function (b, a) { return b.value - a.value })
-        //     .thenBy(function (b, a { return b.size - a.size })
-
-
-        // }
-    )
+        });
 
         let knap = {
             value: 0,
             size: 0,
+            capacity,
             sack: []
         };
 
-        while(knap.size <= capacity ){
-                
-             for(let i = 0; i < items.length; i++) {
+        
+        // while(knap.size <= capacity ){
 
-               return knap.sack = items.slice([i]);
+        // let sack = knap.sack
+        // let knapSize = knap.size += items.size;
+  
+        for(let i = 0; i < items.length; i++) {
 
+            knap.sack = items.slice([i, 1]);
+
+            for(let l = 0; l < knap.sack.length; l++){
+                knap.size += knap.sack[l].size;
             }
 
-        return knap;    
-    }
+            if(knap.size === capacity){
+                return knap;
+            }
 
-        // for(let i = 0; i < knap.sack.length; i++){
+        }
 
-        //     if(knap.sack.length == 0) {
-        //         return 0;
-        //     }
-        //     else{
-        //         return knap.size =+ knap.sack[i].size;
-        //     }
-
-        //     while(knap.size != capacity){
-
-        //         // for(let i = 0; i < items.length; i++){
-
-        //         //     if(items[i].value < items.value){
-                        
-        //         //     }
-        //         // }
-
-        //         items.slice()
-                
-                
-        //     }
-            
-        // }
-
-
-
-
-
-
-    console.log(items);
-
-
-    }
-
-
-    // if (i == 0) {
-    //     return {
-    //       value: 0,
-    //       size: 0,
-    //       chosen: []
-    //     };
-    //   }
-
-    // else if(items[i].size > size) {
-    //     for(let i = items[i].size; i > size; i--)
+        // return knap;
     // }
-    
+}
 
-    
 const argv = process.argv.slice(2);
 
 // add an error check to check the number of params
@@ -139,4 +74,4 @@ for (let l of lines) {
   };
 }
 
-console.log("Naive Recursive implementation: ", loopKnapsack(items, capacity));
+console.log("loopSack implementation: ", loopSack(items, capacity));
