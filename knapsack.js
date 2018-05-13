c85a770edd6f8e7d108124f91523d8cc6825fa33
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-13 10:26:33 -0400

    more console.logs, without much success shortened the list of items hoping to understand what was going on with less input

--- knapsack.js
+++ knapsack.js
@@ -9,8 +9,12 @@ function naiveKnapsack(items, capacity) {
 
   // recursive solution
   function recurse(i, size) {
+    console.log(`recurse() called again, beginning: i: ${i}, size: ${size}`);
     // base case
     if (i == 0) {
+      console.log(
+        `Base Case, RESET all values to [value: 0, size: 0, chosen[]]`
+      );
       return {
         value: 0,
         size: 0,
@@ -26,6 +30,13 @@ function naiveKnapsack(items, capacity) {
     // Pick up an item
     // case: item doesn't fit
     else if (items[i].size > size) {
+      console.log(
+        `item[${i} will not fit] so return {value:${
+          recurse(i - 1, size).value
+        }, size: ${recurse(i - 1, size).size}, chosen:[${
+          recurse(i - 1, size).chosen
+        }]}`
+      );
       return recurse(i - 1, size);
     }
 
@@ -34,18 +45,44 @@ function naiveKnapsack(items, capacity) {
     // have in our bag
     else {
       // the max value we've accumulated so far
-      const r0 = recurse(i - 1, size);
+      let r0 = recurse(i - 1, size);
+      console.log(
+        `r0 values: {value:${r0.value}, size:${r0.size}, chosen:[${r0.chosen}]`
+      );
       // the max value we could have if we added the new item we picked
       // but evicted some others
-      const r1 = recurse(i - 1, size - items[i].size);
+      let r1 = recurse(i - 1, size - items[i].size);
+      console.log(
+        `r1 values: {value:${r1.value}, size:${
+          r1.size
+        } (size(${size}) - item size(${items[i].size})), chosen:[${r1.chosen}]`
+      );
 
       r1.value += items[i].value;
 
-      if (r0.value > r1.value) {
+      if (r0.value >= r1.value) {
+        console.log(`r0 wins: `);
+        console.log(
+          `All three values replaced with r0: {value:${r0.value}, size:${
+            r0.size
+          }, chosen:[${r0.chosen}] INSTEAD OF r1: {value:${r1.value}, size:${
+            r1.size
+          }, chosen:[${r1.chosen}]`
+        );
         return r0;
       } else {
+        console.log(`current index: ${i}`);
+        // .size changed
         r1.size += items[i].size;
+
         r1.chosen = r1.chosen.concat(i);
+        console.log(
+          `All three values replaced with r1: {value:${r1.value}, size:${
+            r1.size
+          }, chosen:[${r1.chosen}] INSTEAD OF r0: {value:${r0.value}, size:${
+            r0.size
+          }, chosen:[${r0.chosen}]`
+        );
         return r1;
       }
     }
@@ -54,140 +91,140 @@ function naiveKnapsack(items, capacity) {
   return recurse(items.length - 1, capacity);
 }
 
-/* Recursive Memoized */
-function knapsackRecursiveMemoized(items, capacity) {
-  const cache = Array(items.length);
+// /* Recursive Memoized */
+// function knapsackRecursiveMemoized(items, capacity) {
+//   const cache = Array(items.length);
 
-  // fill the cache array with more arrays
-  for (let i = 0; i < items.length; i++) {
-    cache[i] = Array(capacity + 1).fill(null);
-  }
+//   // fill the cache array with more arrays
+//   for (let i = 0; i < items.length; i++) {
+//     cache[i] = Array(capacity + 1).fill(null);
+//   }
 
-  // i is the index of the item
-  // size is the capacity
-  function recurseMemoized(i, size) {
-    let answer = cache[i][size];
+//   // i is the index of the item
+//   // size is the capacity
+//   function recurseMemoized(i, size) {
+//     let answer = cache[i][size];
 
-    if (!answer) {
-      answer = recurse(i, size);
-      // make a copy of the answer we got
-      cache[i][size] = Object.assign({}, answer);
-    }
+//     if (!answer) {
+//       answer = recurse(i, size);
+//       // make a copy of the answer we got
+//       cache[i][size] = Object.assign({}, answer);
+//     }
 
-    return answer;
-  }
+//     return answer;
+//   }
 
-  function recurse(i, size) {
-    if (i === 0) {
-      return {
-        size: 0,
-        value: 0,
-        chosen: []
-      };
-    } else if (items[i].size > size) {
-      return recurseMemoized(i - 1, size);
-    } else {
-      const r0 = recurseMemoized(i - 1, size);
-      const r1 = recurseMemoized(i - 1, size - items[i].size);
+//   function recurse(i, size) {
+//     if (i === 0) {
+//       return {
+//         size: 0,
+//         value: 0,
+//         chosen: []
+//       };
+//     } else if (items[i].size > size) {
+//       return recurseMemoized(i - 1, size);
+//     } else {
+//       const r0 = recurseMemoized(i - 1, size);
+//       const r1 = recurseMemoized(i - 1, size - items[i].size);
 
-      r1.value += items[i].value;
+//       r1.value += items[i].value;
 
-      if (r0.value > r1.value) {
-        return r0;
-      } else {
-        r1.size += items[i].size;
-        r1.chosen = r1.chosen.concat(i);
-        return r1;
-      }
-    }
-  }
-  // don't forget to make the inital call to our recursive function
-  return recurseMemoized(items.length - 1, capacity);
-}
+//       if (r0.value > r1.value) {
+//         return r0;
+//       } else {
+//         r1.size += items[i].size;
+//         r1.chosen = r1.chosen.concat(i);
+//         return r1;
+//       }
+//     }
+//   }
+//   // don't forget to make the inital call to our recursive function
+//   return recurseMemoized(items.length - 1, capacity);
+// }
 
-/* Iterative Memoization */
-function knapsackIterative(items, capacity) {
-  const cache = Array(items.length);
+// /* Iterative Memoization */
+// function knapsackIterative(items, capacity) {
+//   const cache = Array(items.length);
 
-  for (let i = 0; i < items.length; i++) {
-    cache[i] = Array(capacity + 1).fill(null);
-  }
+//   for (let i = 0; i < items.length; i++) {
+//     cache[i] = Array(capacity + 1).fill(null);
+//   }
 
-  // seed our cache with base values
-  for (let i = 0; i <= capacity; i++) {
-    cache[0][i] = {
-      size: 0,
-      value: 0,
-      chosen: []
-    };
-  }
+//   // seed our cache with base values
+//   for (let i = 0; i <= capacity; i++) {
+//     cache[0][i] = {
+//       size: 0,
+//       value: 0,
+//       chosen: []
+//     };
+//   }
 
-  // looping through all items;
-  for (let i = 1; i < items.length; i++) {
-    // looping through all capacities
-    for (let j = 0; j <= capacity; j++) {
-      // check if the item fits our size constraint
-      if (items[i].size > j) {
-        // if the item doesn't fit, just use the previous best value
-        cache[i][j] = cache[i - 1][j];
-      } else {
-        // item does fit
-        // look at the previous best value and the value we'll get
-        // by taking the current item. choose the best one.
+//   // looping through all items;
+//   for (let i = 1; i < items.length; i++) {
+//     // looping through all capacities
+//     for (let j = 0; j <= capacity; j++) {
+//       // check if the item fits our size constraint
+//       if (items[i].size > j) {
+//         // if the item doesn't fit, just use the previous best value
+//         cache[i][j] = cache[i - 1][j];
+//       } else {
+//         // item does fit
+//         // look at the previous best value and the value we'll get
+//         // by taking the current item. choose the best one.
 
-        const r0 = cache[i - 1][j];
-        const r1 = Object.assign({}, cache[i - 1][j - items[i].size]);
+//         const r0 = cache[i - 1][j];
+//         const r1 = Object.assign({}, cache[i - 1][j - items[i].size]);
 
-        r1.value += items[i].value;
+//         r1.value += items[i].value;
 
-        if (r0.value > r1.value) {
-          cache[i][j] = r0;
-        } else {
-          r1.size += items[i].size;
-          r1.chosen = r1.chosen.concat(i);
-          cache[i][j] = r1;
-        }
-      }
-    }
-  }
+//         if (r0.value > r1.value) {
+//           cache[i][j] = r0;
+//         } else {
+//           r1.size += items[i].size;
+//           r1.chosen = r1.chosen.concat(i);
+//           cache[i][j] = r1;
+//         }
+//       }
+//     }
+//   }
 
-  return cache[cache.length - 1][capacity];
-}
+//   return cache[cache.length - 1][capacity];
+// }
 
-/* Greedy Implementation */
-function knapsackGreedy(items, capacity) {
-  const answer = {
-    size: 0,
-    value: 0,
-    chosen: []
-  };
+// /* Greedy Implementation */
+// function knapsackGreedy(items, capacity) {
+//   const answer = {
+//     size: 0,
+//     value: 0,
+//     chosen: []
+//   };
 
-  // slice off the first empty entry in our items array
-  // so that we can sort properly
-  items = items.slice(1);
+//   // slice off the first empty entry in our items array
+//   // so that we can sort properly
+//   items = items.slice(1);
 
-  // sort the items array in descending order
-  items.sort((item0, item1) => {
-    const r0 = item0.value / item0.size;
-    const r1 = item1.value / item1.size;
+//   // sort the items array in descending order
+//   items.sort((item0, item1) => {
+//     const r0 = item0.value / item0.size;
+//     const r1 = item1.value / item1.size;
 
-    return r1 - r0;
-  });
+//     return r1 - r0;
+//   });
 
-  // Add items to our knapsack from the top of the items array
+//   // Add items to our knapsack from the top of the items array
 
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].size <= capacity) {
-      answer.size += items[i].size;
-      answer.value += items[i].value;
-      answer.chosen.push(items[i].index);
+//   for (let i = 0; i < items.length; i++) {
+//     if (items[i].size <= capacity) {
+//       answer.size += items[i].size;
+//       answer.value += items[i].value;
+//       answer.chosen.push(items[i].index);
 
-      capacity -= items[i].size;
-    }
-  }
+//       capacity -= items[i].size;
+//     }
+//   }
 
-  return answer;
-}
+//   return answer;
+// }
 
 /* Timing code */
 function timedRun(name, f, items, capacity) {
@@ -236,10 +273,11 @@ for (let l of lines) {
   };
 }
 
-timedRun('Iterative Memoized', knapsackIterative, items, capacity);
+// timedRun('Iterative Memoized', knapsackIterative, items, capacity);
 // timedRun("Recursive Memoized", knapsackRecursiveMemoized, items, capacity);
 // timedRun("Greedy Solution", knapsackGreedy, items, capacity);
-// console.log("Naive Recursive implementation: ", naiveKnapsack(items, capacity));
+console.log('Naive Recursive implementation: ', naiveKnapsack(items, capacity));
+
 // // ========= RATIO SOLUTION - Cesar -- FASTEST (7ms on large) ======= //
 
 // const startTime = Date.now();
