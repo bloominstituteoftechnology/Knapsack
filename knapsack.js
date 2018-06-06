0593608410a65cbbbdb33817d4d06a503422625d
Devon Bacon <devon.bacon@gmail.com>
2018-06-06 15:31:14 -0700

    knapsack.js - set up vars for args

--- knapsack.js
+++ knapsack.js
@@ -6,3 +6,11 @@ if (argv.length != 2) {
   console.error("usage: filename capacity");
   process.exir(1);
 }
+
+const filename = argv[0];
+const capacity = parseInt(argv[1]);
+
+// Reas the file
+const filedata = fs.readFileSync(filename, "utf8");
+// Split the filedata on each new line
+const lines = filedata.trim().split(/[\r\n]+/g);
