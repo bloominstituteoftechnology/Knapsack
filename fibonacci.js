36aec16b7772931a6228abfb11292179fb790695
Devon Bacon <devon.bacon@gmail.com>
2018-06-06 15:38:31 -0700

    fibonacci.js - copied Bottom Up Iterative fibonacci from lecture

--- fibonacci.js
+++ fibonacci.js
@@ -11,7 +11,7 @@
   with the ability to save work we've already done. This doesn't actually
   improve the theoretical runtime complexity over the naive recursive 
   approach, but it does significantly improve the actual running time.
-
+  
   1. Initialize a cache (can be an object or an array)
   2. Write a helper function that checks the cache for the answer we're looking for
   3. If the answer is not found, fall back on our naive logic
@@ -42,4 +42,31 @@ function nthFib(n) {
   return nthFibMemo(n);
 }
 
+/* 
+  Bottom Up Iterative 
+  The idea: Generally follow the same logic as the memoized recursive approach. We 
+  still make use of a cache to save prior data. In this case though, we seed the cache
+  with some initial values and then loop up to our input, along the way populating the
+  cache with the answer for the currennt iteration.
+
+  1. Initialize a cache (again, can be an object or array)
+  2. Seed the cache with initial values so that we can build up from there
+  3. Loop up to our input (don't forget to skip over the initial seed values)
+  4. Populate the cache with the answer for the current iteration
+  5. Return cache[n]
+*/
+
+function nthFibIterative(n) {
+  const cache = Array(n);
+  cache[0] = 0;
+  cache[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    cache[i] = cache[i - 1] + cache[i - 2];
+  }
+
+  return cache[n];
+}
+
+console.log(nthFibIterative(100));
 
