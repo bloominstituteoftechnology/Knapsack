453b683d4347586df31a38fd693be281524d2da1
Devon Bacon <devon.bacon@gmail.com>
2018-06-06 15:34:26 -0700

    knapsack.js - created greadyAlgoSolo to 'fill bag'

--- knapsack.js
+++ knapsack.js
@@ -1,5 +1,27 @@
 const fs = require('fs');
 
+// Solo partial completion before solution lecture
+const greedyAlgoSolo = (items, capacity) => {
+  // Track size, value, and indecies of chosen items
+  let size = 0, value = 0, chosen = [];
+  // Sort based on size/value ratio
+  const sorted = items.slice().sort(((a, b) => {
+    return (b.value / b.size) - (a.value / a.size);
+  }));
+  // Loop through sorted array
+  for(item of sorted) {
+    // Check if item.size will break capacity
+    if(size + item.size <= capacity) {
+        chosen.push(item.index);
+        size += item.size;
+        value += item.value;
+    }
+  }
+
+  return `Value: ${value}, Size: ${size}, Chosen: ${chosen}`;
+}
+
+
 const argv = process.argv.slice(2);
 
 if (argv.length != 2) {
@@ -10,7 +32,7 @@ if (argv.length != 2) {
 const filename = argv[0];
 const capacity = parseInt(argv[1]);
 
-// Reas the file
+// Reads the file
 const filedata = fs.readFileSync(filename, "utf8");
 // Split the filedata on each new line
 const lines = filedata.trim().split(/[\r\n]+/g);
@@ -27,3 +49,8 @@ for (let l of lines) {
     value: value,
   });
 }
+
+// console.log(greedyAlgo(items, 100));
+// console.log(greedyAlgoSolo(items, 100));
+
+console.log(naiveKnapsack(items, capacity));
