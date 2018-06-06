754969909c1533d6fa34aa955401dfbc87fafae4
Devon Bacon <devon.bacon@gmail.com>
2018-06-06 15:35:05 -0700

    knapsack.js - copied solution from lecture

--- knapsack.js
+++ knapsack.js
@@ -1,5 +1,39 @@
 const fs = require('fs');
 
+/*
+  Greedy Strategy
+  0. Go through our items and filter out any items whose size > knapsack's capacity
+  1. 'Score' each item by determining its value/weight ratio
+  2. Sort the items array by each item's ratio such that the items with the best ratio
+  are at the top of the array of items
+  3. Grab items off the top of the items array until we reach our knapsack's full capacity
+*/
+
+// Solution lecture
+const greedyAlgo = (items, capacity) => {
+  const result = { size: 0, value: 0, chosen: [] };
+  // items = items.filter(item => item.size < capacity);
+  items.sort((i1, i2) => {
+    const r1 = i1.value / i1.size;
+    const r2 = i2.value / i2.size;
+    return r2 - r1;
+  });
+  // loop through items array, checking to see if the
+  // item's size <= out total capacity
+  for (let i = 0; i < items.length; i++) {
+    // if it is, add it to our final result
+    if(items[i].size <= capacity) {
+      result.size += items[i].size;
+      result.value += items[i].value;
+      result.chosen.push(items[i].index);
+      // don't forget to decrement our total capacity
+      capacity -= items[i].size;
+    }
+  }
+
+  return `Value: ${result.value}, Size: ${result.size}, Chosen: ${result.chosen}`;
+}
+
 // Solo partial completion before solution lecture
 const greedyAlgoSolo = (items, capacity) => {
   // Track size, value, and indecies of chosen items
