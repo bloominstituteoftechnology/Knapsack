4ec4735f1307e8ba094e85d6af128045a1897205
Mark Wong <fmarkwong@gmail.com>
2018-06-06 11:28:33 -0700

    greedy

--- knapsack.js
+++ knapsack.js
@@ -9,32 +9,78 @@ const fs = require('fs');
   3. Grab items off the top of the items array until we reach our knapsack's full capacity
 */
 
-const argv = process.argv.slice(2);
+/* =========== Main ============= */
 
-if (argv.length != 2) {
-  console.error("usage: filename capacity");
-  process.exit(1);
+const args = parseArguements()
+
+const items = getAllItemsFromFile(args.filename, args.capacity);
+
+fillKnapsackByGreedy(items, args.capacity);
+
+
+/* =========== Functions ============= */
+
+function parseArguements() {
+  const argv = process.argv.slice(2);
+
+  if (argv.length != 2) {
+    console.error("usage: filename capacity");
+    process.exit(1);
+  }
+
+  return {
+    filename: argv[0],
+    capacity: parseInt(argv[1])
+  }
 }
 
-const filename = argv[0];
-const capacity = parseInt(argv[1]);
+function getAllItemsFromFile(filename, capacity) {
 
-// Read the file
-const filedata = fs.readFileSync(filename, "utf8");
-// Split the filedata on each new line
-const lines = filedata.trim().split(/[\r\n]+/g);
+  // Read the file
+  const filedata = fs.readFileSync(filename, "utf8");
+  // Split the filedata on each new line
+  const lines = filedata.trim().split(/[\r\n]+/g);
 
-// Process the lines
-const items = [];
+  // Process the lines
+  const items = [];
 
-for (let l of lines) {
-  const [index, size, value] = l.split(" ").map(n => parseInt(n));
+  for (let l of lines) {
+    const [index, size, value] = l.split(" ").map(n => parseInt(n));
 
-  items.push({
-    index: index,
-    size: size,
-    value: value,
+    if (size > capacity) continue;
+
+    items.push({
+      index: index,
+      size: size,
+      value: value,
+      score: value / size
+    });
+  }
+
+  return items;
+}
+
+function fillKnapsackByGreedy(items, capacity) {
+  let totalValue = 0;
+  let totalSize = 0;
+  let indexList = [];
+
+  items.sort(compareScore).forEach( item => {
+    if (totalSize + item.size <= capacity) {
+      totalValue += item.value;
+      totalSize += item.size;
+      indexList.push(item.index);
+    }
   });
-}
 
-console.log(items);
+  console.log('Greedy');
+  console.log(`Value: ${totalValue}`);
+  console.log(`Size: ${totalSize}`);
+  console.log(`Chosen: ${indexList}`);
+
+  function compareScore(a, b) {
+    if (a.score > b.score) return -1; 
+    if (a.score < b.score) return 1; 
+    return 0;
+  }
+}
