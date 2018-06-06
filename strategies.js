4ffdaa10d53bb9153cb801012ee610fcc635a6bc
Jared Cuffe <jcuffe@gmail.com>
2018-06-06 15:48:11 -0700

    recursive strat implemented

--- strategies.js
+++ strategies.js
@@ -22,6 +22,46 @@ const greedy = (items, capacity) => {
   return result
 }
 
+// O(2^n)
+const recursive = (items, capacity) => {
+  const recurse = (i, spaceLeft) => {
+    // build from this
+    if (i === -1) {
+      return {
+        knapsack: [],
+        value: 0,
+        cost: 0
+      }
+    }
+    const { size, value, index } = items[i]
+
+    // item doesn't fit in knapsack
+    if (size > spaceLeft) {
+      return recurse(i - 1, spaceLeft)
+    } else {
+      const untakenPath = recurse(i - 1, spaceLeft)
+      const takenPath = recurse(i - 1, spaceLeft - size)
+
+      takenPath.value += value
+
+      if (untakenPath.value > takenPath.value) {
+        return untakenPath
+      } else {
+        takenPath.cost += size
+        takenPath.knapsack.push(index)
+        return takenPath
+      }
+    }
+  }
+  result = recurse(items.length - 1, capacity)
+  console.log('RECURSIVE\n\n', result)
+  return result
+}
+
+const iterative = (items, capacity) => {
+
+}
+
 module.exports = {
   greedy,
 }
