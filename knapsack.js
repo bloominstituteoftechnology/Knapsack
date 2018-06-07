875c75a564c51148aae72261b53b05d10f367e7c
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-06-07 12:48:59 -0700

    NaiveKnapsack Code works

--- knapsack.js
+++ knapsack.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const argv = process.argv.slice(2);
 /*
   Strategy 1
   0. Go through our items and filter out any items whose size > knapsack's capacity
@@ -9,106 +9,74 @@ const fs = require('fs');
   3. Grab items off the top of the items array until we reach out knapsack's full capacity
 */
 
-//  SEAN's SOLUTION  //
-const greedyAlgo = (items, capacity) => {
-  const result = {
-    size: 0,
-    value: 0,
-    chosen: [],
+/*  AMANDA'S SOLUTION  */
+
+function naiveKnapsack(items, capacity) {
+  // go through each obj
+  // divide its size / value
+  // store ratios
+  let ratioArr = [];
+  for (let i = 0; i < items.length; i++) {
+    // console.log('item at index', items[i].index, items[i].size, 'size');
+    // console.log('ratio', items[i].size / items[i].value);
+    ratioArr[items[i].index] = {
+      index: items[i].index,
+      ratio: items[i].size / items[i].value,
+      size: items[i].size,
+      value: items[i].value,
+    };
+  }
+  ratioArr.shift();
+
+  // order ratios
+  const sortByRatio = ratioArr.slice(0);
+  sortByRatio.sort((a, b) => {
+    return a.ratio - b.ratio;
+  });
+  console.log('sortbyratio', sortByRatio);
+
+  // Items to select: 2, 8, 10
+  // Total cost: 98
+  // Total value: 117
+
+  const initKnapsack = {
+    initValue: 0,
+    initCost: 0,
+    itemsSelected: [],
   };
 
-  // items = item.filter(item => item.size < capacity);
-  items.sort((i1, i2) => {
-    const r1 = i1.value / i1.size;
-    const r2 = i2.value / i2. size;
-
-    return r2 - r1;
-  });
-
-// Loop through items array, checking to see if the item's
-// size <= our total capacity
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].size <= capacity) {
-// If it is, add it to our final result
-      result.size += items[i].size;
-      result.value += items[i].value;
-      result.chosen.push(items[i].index);
-// Don't forget to decrement our total capacity
-      capacity -= items[i].size;
+  let sack = capacity;
+  let value = 0;
+  while (sack >= 0) {
+    for (let obj of sortByRatio) {
+      if (sack - obj.size > 0) {
+        sack -= obj.size;
+        value += obj.value;
+        initKnapsack.itemsSelected.push(obj.index);
+        filledKnapsack = {
+          ...initKnapsack,
+          Value: value,
+          Size: capacity - sack,
+        };
+      }
     }
+    return filledKnapsack;
   }
+  // go through each ratio
+  // while capacity under
+  // hold ratio index
 
-  return result;
-};
+  // return items meeting knapsack reqs
+  return 'KNAPSACK SUCCESS';
+}
 
-//  SOLUTION  //
-// function naiveKnapsack(items, capacity) {
-//   // go through each obj
-//   // divide its size / value
-//   // store ratios
-//   let ratioArr = [];
-//   for (let i = 0; i < items.length; i++) {
-//     // console.log('item at index', items[i].index, items[i].size, 'size');
-//     // console.log('ratio', items[i].size / items[i].value);
-//     ratioArr[items[i].index] = {
-//       index: items[i].index,
-//       ratio: items[i].size / items[i].value,
-//       size: items[i].size,
-//       value: items[i].value,
-//     };
-//   }
-//   ratioArr.shift();
-
-//   // order ratios
-//   const sortByRatio = ratioArr.slice(0);
-//   sortByRatio.sort((a, b) => {
-//     return a.ratio - b.ratio;
-//   });
-//   console.log('sortbyratio', sortByRatio);
-
-//   // Items to select: 2, 8, 10
-//   // Total cost: 98
-//   // Total value: 117
-
-//   const initknapsack = {
-//     itemsSelected: [],
-//     totalCost: 0,
-//     totalValue: 0,
-//   };
-//   let sack = capacity;
-//   let value = 0;
-//   while (sack >= 0) {
-//     for (let obj of sortByRatio) {
-//       if (sack - obj.size > 0) {
-//         sack -= obj.size;
-//         value += obj.value;
-//         initKnapsack.itemsSelected.push(obj.index);
-//         filledKnapsack = {
-//           ...initKnapsack,
-//           totalCost: capacity - sack,
-//           totalValue: value,
-//         };
-//       }
-//     }
-//     return filledKnapsack;
-//   }
-//   // go through each ratio
-//   // while capacity under
-//   // hold ratio index
-
-//   // return items meeting knapsack reqs
-//   return 'meh';
-// }
-
-const argv = process.argv.slice(2);
-
-if (argv.length != 2) {
+if (argv.length !== 2) {
   console.error("usage: filename capacity");
   process.exit(1);
 }
 
 const filename = argv[0];
-const capacity = parseInt(argv[1]);
+const capacity = argv[1];
 
 // Read the File
 const filedata = fs.readFileSync(filename, "utf8");
@@ -122,51 +90,109 @@ const items = [];
 for (let l of lines) {
   const [index, size, value] = l.split(" ").map(n => parseInt(n));
 
-// - Value to Size Ratio
-  // const retValue = value / size
-  // return { index, size, value, retValue }
-
-// const items = lines.map(line => {
-//   const [index, size, value] = line.split(' ')
-//   const relValue = value / size
-//   return { index, size, value, relValue }
-// })
-
-// - Sorting Items than return remaing capacity
-// items.sort((a, b) => {
-//   return b.retValue - a.retValue
-// })
-
-// - Knapsack Max Capacity
-// const knapsack = {
-//   contains: [],
-//   totalSize: Number(0),
-//   totalValue: Number(0),
-//   currCap: Number(capacity)
-// }
-
-// - Knapsack Calculations
-// for (let i = 0; i < items.length; i++) {
-//   if(items[i].size <= knapsack.currCap) {
-//     knapsack.contains.push(items[i].index)
-//     knapsack.totalSize += Number(items[i].size)
-//     knapsack.totalValue += Number(items[i].value)
-//     knapsack.currCap -= Number(items[i].size)
-//   }
-// }
-
-  items.push({
-    index: index,
-    size: size,
-    value: value,
-  });
+  items[index] = {
+    index,
+    size,
+    value,
+  };
 }
 
-// return knapsack
+items.shift();
 
-console.log("Naive Recursive implementation: ", greedyAlgo(items, capacity));
+console.log("Naive Recursive implementation: ", naiveKnapsack(items, capacity));
 
-//  RECURSIVE SOLUTION  //
+// //  SEAN's SOLUTION  //
+// // const greedyAlgo = (items, capacity) => {
+// //   const result = {
+// //     size: 0,
+// //     value: 0,
+// //     chosen: [],
+// //   };
+
+// //   // items = item.filter(item => item.size < capacity);
+// //   items.sort((i1, i2) => {
+// //     const r1 = i1.value / i1.size;
+// //     const r2 = i2.value / i2. size;
+
+// //     return r2 - r1;
+// //   });
+
+// // // Loop through items array, checking to see if the item's
+// // // size <= our total capacity
+// //   for (let i = 0; i < items.length; i++) {
+// //     if (items[i].size <= capacity) {
+// // // If it is, add it to our final result
+// //       result.size += items[i].size;
+// //       result.value += items[i].value;
+// //       result.chosen.push(items[i].index);
+// // // Don't forget to decrement our total capacity
+// //       capacity -= items[i].size;
+// //     }
+// //   }
+
+// //   return result;
+// // };
+
+// //  SOLUTION  //
+// // function naiveKnapsack(items, capacity) {
+// //   // go through each obj
+// //   // divide its size / value
+// //   // store ratios
+// //   let ratioArr = [];
+// //   for (let i = 0; i < items.length; i++) {
+// //     // console.log('item at index', items[i].index, items[i].size, 'size');
+// //     // console.log('ratio', items[i].size / items[i].value);
+// //     ratioArr[items[i].index] = {
+// //       index: items[i].index,
+// //       ratio: items[i].size / items[i].value,
+// //       size: items[i].size,
+// //       value: items[i].value,
+// //     };
+// //   }
+// //   ratioArr.shift();
+
+// //   // order ratios
+// //   const sortByRatio = ratioArr.slice(0);
+// //   sortByRatio.sort((a, b) => {
+// //     return a.ratio - b.ratio;
+// //   });
+// //   console.log('sortbyratio', sortByRatio);
+
+// //   // Items to select: 2, 8, 10
+// //   // Total cost: 98
+// //   // Total value: 117
+
+// //   const initknapsack = {
+// //     itemsSelected: [],
+// //     totalCost: 0,
+// //     totalValue: 0,
+// //   };
+// //   let sack = capacity;
+// //   let value = 0;
+// //   while (sack >= 0) {
+// //     for (let obj of sortByRatio) {
+// //       if (sack - obj.size > 0) {
+// //         sack -= obj.size;
+// //         value += obj.value;
+// //         initKnapsack.itemsSelected.push(obj.index);
+// //         filledKnapsack = {
+// //           ...initKnapsack,
+// //           totalCost: capacity - sack,
+// //           totalValue: value,
+// //         };
+// //       }
+// //     }
+// //     return filledKnapsack;
+// //   }
+// //   // go through each ratio
+// //   // while capacity under
+// //   // hold ratio index
+
+// //   // return items meeting knapsack reqs
+// //   return 'meh';
+// // }
+
+// //  RECURSIVE SOLUTION  //
 // function naiveKnapsack(items, capacity) {
 //   // What is the value we have when we don't pick up any items
 //   // value[0, w] = 0
@@ -174,7 +200,7 @@ console.log("Naive Recursive implementation: ", greedyAlgo(items, capacity));
   
 //   function recurse(i, size) {
 //     // Base case
-//     if (i == 0) {
+//     if (i === -1) {
 //       return {
 //         value: 0,
 //         size: 0,
@@ -210,7 +236,7 @@ console.log("Naive Recursive implementation: ", greedyAlgo(items, capacity));
 //         return r0;
 //       } else {
 //         r1.size += items[i].size;
-//         r1.chosen = r1.chosen.concat(i);
+//         r1.chosen = r1.chosen.concat(i + 1);
 //         return r1;
 //       }
 //     }
@@ -218,3 +244,69 @@ console.log("Naive Recursive implementation: ", greedyAlgo(items, capacity));
 
 //   return recurse(items.length - 1, capacity);
 // }
+
+// const argv = process.argv.slice(2);
+
+// if (argv.length != 2) {
+//   console.error("usage: filename capacity");
+//   process.exit(1);
+// }
+
+// const filename = argv[0];
+// const capacity = parseInt(argv[1]);
+
+// // Read the File
+// const filedata = fs.readFileSync(filename, "utf8");
+
+// // Split the filedata on each new line
+// const lines = filedata.trim().split(/[\r\n]+/g);
+
+// // Process the lines
+// const items = [];
+
+// for (let l of lines) {
+//   const [index, size, value] = l.split(" ").map(n => parseInt(n));
+
+// // - Value to Size Ratio
+//   // const retValue = value / size
+//   // return { index, size, value, retValue }
+
+// // const items = lines.map(line => {
+// //   const [index, size, value] = line.split(' ')
+// //   const relValue = value / size
+// //   return { index, size, value, relValue }
+// // })
+
+// // - Sorting Items than return remaing capacity
+// // items.sort((a, b) => {
+// //   return b.retValue - a.retValue
+// // })
+
+// // - Knapsack Max Capacity
+// // const knapsack = {
+// //   contains: [],
+// //   totalSize: Number(0),
+// //   totalValue: Number(0),
+// //   currCap: Number(capacity)
+// // }
+
+// // - Knapsack Calculations
+// // for (let i = 0; i < items.length; i++) {
+// //   if(items[i].size <= knapsack.currCap) {
+// //     knapsack.contains.push(items[i].index)
+// //     knapsack.totalSize += Number(items[i].size)
+// //     knapsack.totalValue += Number(items[i].value)
+// //     knapsack.currCap -= Number(items[i].size)
+// //   }
+// // }
+
+//   items.push({
+//     index: index,
+//     size: size,
+//     value: value,
+//   });
+// }
+
+// // return knapsack
+
+// console.log("Naive Recursive implementation: ", naiveKnapsack(items, capacity));
