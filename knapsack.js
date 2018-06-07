1998a0ab326904781d935a72d4a78102047e6eba
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-06-07 16:33:44 -0400

    Completed the memoized recursive strategy

--- knapsack.js
+++ knapsack.js
@@ -27,50 +27,89 @@ const lines = filedata.trim().split(/[\r\n]+/g);
 // Process the lines
 const items = [];
 
-// Greedy Strategy
-const greedy = () => {
-    for (let l of lines) {
-    const [index, size, value] = l.split(" ").map(n => parseInt(n));
+for (let l of lines) {
+  const [index, size, value] = l.split(" ").map(n => parseInt(n));
 
-    items.push({
-        index,
-        size,
-        value
-    });
-    }
-
-    const sorter = () => {
-    items.forEach(n => {
-        n["score"] = n.value / n.size;
-    });
-    items.sort((a, b) => {
-        // if (a.score < b.score) { return -1 }
-        // else if (a.score > b.score) { return 1 }
-        // else { return 0 }
-        return b.score - a.score;
-    });
-    };
-
-    sorter();
-    // console.log(items);
-
-    let knapsack = [];
-    let value = 0;
-    let cost = 0;
-
-    items.forEach(item => {
-    if (item.size <= capacity) {
-        knapsack.push(item.index);
-        capacity -= item.size;
-        cost += item.size;
-        value += item.value;
-    }
-    });
-
-    console.log("\nGreedy method");
-    console.log("Items to select:", knapsack);
-    console.log("Total Cost: ", cost);
-    console.log("Total value: ", value, "\n");
+  items.push({
+    index,
+    size,
+    value
+  });
 }
 
-greedy()
+// Greedy Strategy
+const greedy = () => {
+
+  const sorter = () => {
+    items.forEach(n => {
+      n["score"] = n.value / n.size;
+    });
+    items.sort((a, b) => {
+      // if (a.score < b.score) { return -1 }
+      // else if (a.score > b.score) { return 1 }
+      // else { return 0 }
+      return b.score - a.score;
+    });
+  };
+
+  sorter();
+  console.log(items);
+
+  let knapsack = [];
+  let value = 0;
+  let cost = 0;
+
+  items.forEach(item => {
+    if (item.size <= capacity) {
+      knapsack.push(item.index);
+      capacity -= item.size;
+      cost += item.size;
+      value += item.value;
+    }
+  });
+
+  console.log("\nGreedy method");
+  console.log("Items to select:", knapsack);
+  console.log("Total Cost: ", cost);
+  console.log("Total value: ", value, "\n");
+};
+
+// Memoized Recursive Strategy
+const memoized = (items, capacity) => {
+  const base = {
+    takenItems: [],
+    size: 0,
+    value: 0
+  }
+
+  const recurse = (i, remainingSize) => {
+    if (i === - 1) {
+      return {
+        takenItems: [],
+        size: 0,
+        value: 0
+      }
+    } 
+
+    if (items[i].size > remainingSize) {
+      return recurse(i - 1, remainingSize)
+    } else {
+      const untakenItem = recurse(i - 1, remainingSize);
+      const takenItem = recurse(i - 1, remainingSize - items[i].size);
+
+      takenItem.value += items[i].value;
+
+      if (takenItem.value > untakenItem.value) {
+        takenItem.size += items[i].size;
+        takenItem.takenItems.push(items[i].index);
+        return takenItem;
+      } else {
+        return untakenItem;
+      }
+    }
+  }
+  return recurse(items.length - 1, capacity)
+}
+
+// greedy();
+console.log(memoized(items, capacity));
