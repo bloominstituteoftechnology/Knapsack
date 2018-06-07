8e14184c90e8e128e096defec664d8a0a343092d
Justin Hong <jhong2009@gmail.com>
2018-06-07 16:02:31 -0700

    in knapsack.js: attempted solution to memoized recursive strategy

--- knapsack.js
+++ knapsack.js
@@ -1,5 +1,66 @@
 const fs = require('fs');
 
+/* 
+  Memoized Recursive Strategy
+  1. Initialize a cache (can be an object or an array)
+  2. Write a helper function that checks the cache for the answer we're looking for
+  3. If the answer is not found, fall back on our naive logic
+  4. The naive helper needs to recursively call the memoized version, not itself
+  5. Return the value that the memoized function returns
+*/
+
+function knapsack(items, capacity) {
+  const cache = Array(items.length).fill(Array(capacity + 1).fill(null)); // this will be a matrix, 2-dimensional array
+  // add the second dimension
+  // for (let i = 0; i < items.length; i++) {
+  //   cache[i] = Array(capacity + 1).fill(null);
+  // }
+  // check cache
+  function recurseMemo(i, size) {
+    // i - iteration number
+    // size - remaining capacity
+    let value = cache[i][size];
+
+    if (!value) {
+      value = recurseNaive(i, size);
+      cache[i][size] = Object.assign({}, value);
+    }
+
+    return value;
+  }
+
+  function recurseNaive(i, size) {
+    if (i === -1) {
+      return {
+        value: 0,
+        size: 0,
+        chosen: [],
+      };
+    }
+
+    // check to see if the item fits
+    else if (items[i].size > size) {
+      return recurseMemo(i - 1, size);
+    }
+    // Item fits, but might not be worth as much as items in there already
+    else {
+      const r0 = recurseMemo(i - 1, size);
+      const r1 = recurseMemo(i - 1, size - items[i].size);
+
+      r1.value += items[i].value;
+
+      if (r0.value > r1.value) {
+        return r0;
+      } else {
+        r1.size += items[i].size;
+        r1.chosen = r1.chosen.concat(i + 1);
+        return r1;
+      }
+    }
+  }
+  return recurseMemo(items.length - 1, capacity);
+}
+
 /*
   Greedy Strategy
   0. Go through our items and filter out any items whose size > knapsack's capacity
@@ -8,19 +69,6 @@ const fs = require('fs');
   are at the top of the array of items
   3. Grab items off the top of the items array until we reach our knapsack's full capacity
 */
-
-// const printOut = (knapsack) => {
-//   let items = `Items to Select: `;
-//   let totalCost = 0;
-//   let totalValue = 0;
-
-//   knapsack.forEach(item => {
-//     items += `${item.index} `;
-//     totalCost += item.size;
-//     totalValue += item.value;
-//   });
-//   return `${items}\nTotal cost: ${totalCost}\nTotal value: ${totalValue}`;
-// }
 
 const greedyAlgo = (items) => {
   const result = {
@@ -87,4 +135,5 @@ for (let l of lines) {
   });
 }
 
-console.log(greedyAlgo(items));
+// console.log(greedyAlgo(items));
+console.log(knapsack(items, capacity));
