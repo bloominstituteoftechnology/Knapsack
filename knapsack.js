59c8515a2d2936d038a7e1bd333f766133ac12ff
Henry K <mykim1891@hotmail.com>
2018-06-07 16:23:37 -0700

    Updates with solution code for memoized Recursive function

--- knapsack.js
+++ knapsack.js
@@ -18,8 +18,79 @@ function naiveKnapsack(items, capacity) {
     }
     // Item fits, but might not be worth as much as items in there already
     else {
+      // 2 different recursive cases
+      // this one moves on to next item without changes
       const r0 = recurse(i - 1, size);
+      console.log("r0", r0, i);
+      // this one puts the item in the bag; essentially capacity - size of item
       const r1 = recurse(i - 1, size - items[i].size);
+
+      // defining case when value of r1
+      r1.value += items[i].value;
+      console.log("r1", r1, i);
+
+      if (r0.value > r1.value) {
+        console.log("r0 returned");
+        return r0;
+      } else {
+        r1.size += items[i].size;
+        r1.chosen = r1.chosen.concat(i + 1);
+        console.log("r1 returned");
+        return r1;
+      }
+    }
+  }
+  return recurse(items.length - 1, capacity);
+}
+
+/* Memoized Rercursive */
+function memoizedKnapsack(items, capacity) {
+  // console.log(items.length, capacity);
+  // initalize cache (in this, it will be a matrix)
+  const cache = Array(items.length).fill(Array(capacity + 1).fill(null));
+
+  // add the second dimension
+  // for (let i = 0; i < items.length; i++) {
+  //   cache[i] = Array(capacity + 1).fill(null);
+  // }
+
+  function recurseMemo(i, capacityLeft) {
+    if (i === -1) {
+      return {
+        value: 0,
+        size: 0,
+        chosen: [],
+      };
+    }
+
+    let value = cache[i][capacityLeft];
+
+    if (!value) {
+      value = recurseNaive(i, capacityLeft);
+      cache[i][capacityLeft] = Object.assign({}, value);    // make a copy
+    }
+
+    return value;
+  }
+
+  function recurseNaive(i, capacityLeft) {
+    if (i === -1) {
+      return {
+        value: 0,
+        size: 0,
+        chosen: [],
+      };
+    }
+    // check to see if the item fits
+    else if (items[i].size > capacityLeft) {
+      return recurseMemo(i - 1, capacityLeft);
+    }
+    // Item fits, but might not be worth as much as items in there already
+    // But is it worth taking? Does it positively affect our value?
+    else {
+      // The value we get from not taking the item
+      const r0 = recurseMemo(i - 1, capacityLeft);
+      const r1 = recurseMemo(i - 1, capacityLeft - items[i].size)
 
       r1.value += items[i].value;
 
@@ -27,12 +98,12 @@ function naiveKnapsack(items, capacity) {
         return r0;
       } else {
         r1.size += items[i].size;
-        r1.chosen = r1.chosen.concat(i + 1);
+        r1.chosen = r1.chosen.concat(items[i].index);
         return r1;
       }
     }
   }
-  return recurse(items.length - 1, capacity);
+  return recurseMemo(items.length - 1, capacity); 
 }
 
 /*
@@ -101,5 +172,6 @@ for (let l of lines) {
   });
 }
 
-console.log(greedyAlgo(items, capacity));
+// console.log(greedyAlgo(items, capacity));
+console.log("answer:", memoizedKnapsack(items, capacity));
 // console.log(naiveKnapsack(items, capacity));
