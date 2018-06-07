bdc0e117f5f3e3b4208240f12c64fd8937277bd5
Yasin Shuman <email@yasin.io>
2018-06-07 01:05:13 -0700

    followed example for greedy solution

--- knapsack.js
+++ knapsack.js
@@ -9,6 +9,36 @@ const fs = require("fs");
   3. Grab items off the top of the items array until we reach our knapsack's full capacity
 */
 
+const greedyAlgo = (items, capacity) => {
+  const result = {
+    size: 0,
+    value: 0,
+    chosen: []
+  };
+
+  // items = items.filter(item => item.size < capacity);
+
+  //sort by best size/value ratios
+  items.sort((item1, item2) => {
+    const ratio1 = item1.value / item1.size;
+    const ratio2 = item2.value / item2.size;
+    return ratio2 - ratio1;
+  });
+
+  //looping through array checking to see if item size is less than or equal to capacity
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].size <= capacity) {
+      //if it is adding it to final result
+      result.size += items[i].size;
+      result.value += items[i].value;
+      result.chosen.push(items[i].index);
+      //then we decrement capacity to update available size
+      capacity -= items[i].size;
+    }
+  }
+  return result;
+};
+
 const argv = process.argv.slice(2);
 
 if (argv.length != 2) {
@@ -36,3 +66,5 @@ for (let l of lines) {
     value: value
   });
 }
+
+console.log(greedyAlgo(items, capacity));
