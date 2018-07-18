90a4cff6616cf4f9e54f20571b66ce455a3e1702
kelly <kelfro@fastmail.com>
2018-07-18 15:11:25 -0700

    pseudocoding another algorithm

--- knapsack.py
+++ knapsack.py
@@ -8,27 +8,52 @@ Item = namedtuple('Item', ['index', 'size', 'value'])
 def knapsack_solver(items, capacity, index=0, value=0, chosen=[]):
     # !!!! IMPLEMENT ME
     # Stop recursing!
+
     if index >= len(items):
         return[value, chosen] 
+    
     # since you have an empty bag, start by checking the first item
     # does it fit? If not, recurse.
+  
     if items[index].size > capacity:
         return knapsack_solver(items, capacity, index + 1, value, chosen)
-    # if it does, check the value we get from NOT taking the item vs
-    # the value we get from taking it.        
+ 
+    # if it does, check the value we get from NOT taking the item
+    # (novalue) vs the value we get from taking it (yesvalue).
+
     else:
+    # trying to not ruin the actual array/list 
         chosencopy = chosen.copy()
         chosencopy.append(items[index].index)
  
         novalue = knapsack_solver(items, capacity, index+1, value, chosen)
         yesvalue = knapsack_solver(items, capacity - items[index].size, index+1, value + items[index].value, chosencopy)
-        print(novalue)
-        print(yesvalue)
+    #    print(novalue)
+    #    print(yesvalue)
         if novalue[0] > yesvalue[0]:
             return novalue 
         else:
            return yesvalue 
                     
+def anotherks(items, capacity):
+
+    # calculate value/size ratio for each item to determine optimal results
+    # sort items list so that optimal results come first
+    # take items off the top and put into your knapsack until it's full
+
+    # So I think first you make an empty array/list and set initial values to
+    # zero for starters. 
+    chosen = []
+    size = 0
+    value = 0
+
+    # find the value/size ratio for each item
+    # sort chosen array so that highest ratios come first
+
+    # then as long as there is room in the bag (capacity), add items to the
+    # chosen array. Each time you add an item be sure to add the size and value
+    # to the total count so you don't overfill your bag.
+
 
 
 if __name__ == '__main__':
