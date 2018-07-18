1e63efadaa811f622f89022e55e7d074553fde3b
A.D.Faris <adfaris@gmail.com>
2018-07-18 19:00:47 -0400

    Greedy javascript

--- knapsack.py
+++ knapsack.py
@@ -4,8 +4,23 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME
-  pass
+  def recursive(i,size):
+    if i == 0:
+      return value = 0, size = 0, chosen:[]
+    elif items.size > size:
+      return recursive(i-1, size)
+    else:
+      r0 = recursive(i-1, size)
+      r1 = recursive(i-1, size-items[i].size)
+      r1.value += items[i].value
+    if r0.value > r1.value:
+      return r0
+    else:
+      r1.size += items[i].size
+      r1.chosen = r1.chosen.concat(i)
+      return r1
+    return recursive(len(items-1), capacity)
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
