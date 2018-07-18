497a824ec26b2d9e36fe0287e62263ea78bac52c
William VanDolah <william.vandolah@gmail.com>
2018-07-18 16:41:43 -0500

    added comments

--- knapsack.py
+++ knapsack.py
@@ -3,22 +3,28 @@
 import sys
 from collections import namedtuple
 
+# added a value of 'rank' this allows me to sort items by worth
 Item = namedtuple('Item', ['index', 'size', 'value', 'rank'])
 
 def knapsack_solver(items, capacity):
+  # sorting by worth
   worth = sorted(items, key=lambda x: x.rank, reverse=True)
+  # hold remaining capacity
   capacity_left = capacity
+  #holds answers
   items_carried = []
   total_value = 0
   total_size = 0
 
+  # this loop starts at the most valuable item and checks if it will fit
+  # into the knapsack. it it will, the values are added
   for i in worth:
     if capacity_left - i.size >= 0:
       items_carried.append(i.index)
       capacity_left -= i.size
       total_value += i.value
       total_size += i.size
-  # print(total_value, total_size, items_carried)
+
   return "Value: {} \nSize: {} \nChosen: {}".format(total_value, total_size, items_carried)
 
   
