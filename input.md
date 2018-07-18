3ad2f571c2caf9e262dde658a74c8c1b5d47e438
mister-corn <code@ronaldlibago.com>
2018-07-18 15:45:36 -0700

    My brute force doesn't work, but my other solution does.
    * My BF implementation seems to work for some sets, but begins to fail for larger sets. It's not checking every possibility it seems. :/
    * Optimized is actually working for all tests. The time complexity for the logic itself is O(n), but the list needs to be sorted by ratio, which def adds to the time complexity. Need to look up by how much.

---
+++ input.md
@@ -0,0 +1,68 @@
+1 42 81
+2 42 42
+3 68 56
+4 68 25
+5 77 14
+6 57 63
+7 17 75
+8 19 41
+9 94 19
+10 34 12
+
+---
+
+The goal is to select a subset of the items to maximize the payoff such that the cost is below some threshold. 
+
+1, value = 70, weight = 25
+2, value = 50, weight = 10
+
+Max weight your bag can carry = 25
+
+Combination   Total Value Total Weight Valid?
+1             70          25            Yes, 25 = 25
+1 2           120         35            No, 35 > 25
+2             50          10            Yes, 10 < 25
+2 1           120         35            No, 35 > 25
+
+---
+
+Givens:
+```py
+items = [(item, weight, value)]
+max_weight = 25
+
+best_items = []
+best_value = 0
+
+for i in range(0,1):
+  knapsack = []
+  value_in_knapsack = 0
+  weight_of_knapsack = 0
+
+  if items[i].weight <= max_weight:
+    knapsack.append(items[i])
+    value_in_knapsack += items[i].value
+    weight_of_knapsack += items[i].weight
+  
+  if best_value < value_in_knapsack:
+    best_value = value_in_knapsack
+    best_items = knapsack
+  
+  for j in range(0,1):
+    if items[i] != items[j]:
+      if items[i].weight + weight_of_knapsack <= max_weight:
+        knapsack.append(items[i])
+        value_in_knapsack += items[i].value
+    
+      if best_value < value_in_knapsack:
+        best_value = value_in_knapsack
+        best_items = knapsack
+  
+```
+
+
+  
+
+```
+n*(n+1)/2 = 3*(4)/2 = 6
+
--- knapsack.py
+++ knapsack.py
@@ -3,9 +3,9 @@
 import sys
 from collections import namedtuple
 
-Item = namedtuple('Item', ['index', 'size', 'value'])
+Item = namedtuple('Item', ['index', 'size', 'value', 'ratio'])
 
-def knapsack_solver(items, capacity):
+def knapsack_solver_maybe(items, capacity):
   """!!!! IMPLEMENT ME"""
   # set = []
   # size = 0
@@ -21,6 +21,11 @@ def knapsack_solver(items, capacity):
     set.append(itemA)
     size += itemA.size
     value += itemA.value
+    print('current set A:',[item.index for item in set])
+
+    if value > best_value:
+      best_set = set
+      best_value = value
 
     for itemB in items:
       if itemA != itemB:
@@ -28,6 +33,7 @@ def knapsack_solver(items, capacity):
           set.append(itemB)
           size += itemB.size
           value += itemB.value
+          print('current set B:',[item.index for item in set])
       # print('current set:',set)
 
     if value > best_value:
@@ -39,6 +45,20 @@ def knapsack_solver(items, capacity):
 Total cost: {sum([item.size for item in best_set])}
 Total value: {best_value}'''
 
+def knapsack_solver(items, capacity):
+  sorted_items = sorted(items, key=lambda item: item.ratio, reverse=True)
+  best_set = []
+  current_size = 0
+
+  for item in sorted_items:
+    if item.size + current_size <= capacity:
+      best_set.append(item)
+      current_size += item.size
+      print('best set so far:',best_set)
+  
+  return f'''\nItems to select: {sorted([item.index for item in best_set])}
+Total cost: {sum([item.size for item in best_set])}
+Total value: {sum([item.value for item in best_set])}'''
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
@@ -49,7 +69,7 @@ if __name__ == '__main__':
 
     for line in file_contents.readlines():
       data = line.rstrip().split()
-      items.append(Item(int(data[0]), int(data[1]), int(data[2])))
+      items.append(Item(int(data[0]), int(data[1]), int(data[2]), int(data[2])/int(data[1])))
     
     file_contents.close()
     print(knapsack_solver(items, capacity))
