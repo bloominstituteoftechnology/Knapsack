a4b9f2b6dd04b98daf021d5ea763b44479a67a08
april <martinezapril56@yahoo.com>
2018-07-18 14:58:12 -0400

    commit

--- knapsack.py
+++ knapsack.py
@@ -6,7 +6,25 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME
+  # !!!! IMPLEMENT 
+  if toConsider[] or avail == 0:
+          result = (0, ())
+      elif toConsider[0].getUnits() > avail:
+          result = maxVal(toConsider[1:], avail)
+      else:
+          nextItem = toConsider[0]
+          withVal, withToTake = maxVal(toConsider[1:]),
+                                    avail - nextItem.getUnits())
+          withVal += nextItem.getValue()
+          withoutVal, withoutToTake = maxVal(toConsider[1:], avail)
+      if withVal > withoutVal:
+              result = (withVal, withToTake + (nextItem,))
+          else:
+                
+              result = (withoutVal, withoutToTake)
+      return result                                 
+
+  
   pass
 
 if __name__ == '__main__':
