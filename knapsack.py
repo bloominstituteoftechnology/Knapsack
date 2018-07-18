f95fb433fb524bcf175b65e13b7b9f79abe69c6e
Danny Vail <danny_vail@outlook.com>
2018-07-18 19:25:50 -0400

    Tweaked brute force method using a weight to size grade for each item. Works now for all data sets

--- knapsack.py
+++ knapsack.py
@@ -8,47 +8,27 @@ Item = namedtuple('Item', ['index', 'size', 'value'])
 
 # V1 - Brute force
 def knapsack_solver(items, capacity):
-    items_by_value = sorted(items, key=lambda item: item.value, reverse=True)
-    items_by_size = sorted(items, key=lambda item: item.size)
-    knapsack_by_value = {
+    counter = 0
+    sorted_items = sorted(items, key=lambda item: item.value/item.size, reverse=True)
+    knapsack = {
         'value': 0,
         'size': 0,
         'chosen': [],
         'capacity': capacity
     }
-    knapsack_by_size = {
-        'value': 0,
-        'size': 0,
-        'chosen': [],
-        'capacity': capacity
-    }
-
-    # Add most valuable items until no more can fit
-    counter = 0
-    while counter < len(items_by_value):
-        if items_by_value[counter].size <= knapsack_by_value['capacity']:
-            item = items_by_value.pop(counter)
-            knapsack_by_value['value'] += item.value
-            knapsack_by_value['size'] += item.size
-            knapsack_by_value['capacity'] -= item.size
-            knapsack_by_value['chosen'].append(item.index)
+    
+    # Add best weight/value items until no more can fit
+    while counter < len(sorted_items):
+        if sorted_items[counter].size <= knapsack['capacity']:
+            item = sorted_items.pop(counter)
+            knapsack['value'] += item.value
+            knapsack['size'] += item.size
+            knapsack['capacity'] -= item.size
+            knapsack['chosen'].append(item.index)
         else:
             counter += 1
-
-    # Add lightest items until no more can fit
-    counter = 0
-    while counter < len(items_by_size):
-        if items_by_size[counter].size <= knapsack_by_size['capacity']:
-            item = items_by_size.pop(counter)
-            knapsack_by_size['value'] += item.value
-            knapsack_by_size['size'] += item.size
-            knapsack_by_size['capacity'] -= item.size
-            knapsack_by_size['chosen'].append(item.index)
-        else:
-            counter += 1
-
-    # Return knapsack with the highest value
-    return knapsack_by_value if knapsack_by_value['value'] > knapsack_by_size['value'] else knapsack_by_size
+    
+    return knapsack
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
