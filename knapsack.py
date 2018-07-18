1e5dd1b686cf490d819576cf674eb2b7fa0f5185
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-07-18 16:15:27 -0700

    Chore: restart Memoized with starter code
     - deleted wrong approach and restarted after Q&A session

--- knapsack.py
+++ knapsack.py
@@ -10,19 +10,52 @@ def knapsack_solver(items, capacity):
   # Brute Force
   #   add size of 'all' combinations less than the capacity
   #   find the largest value
-  indice = []
-  i = 0
-  j = 0
+  # indice = []
+  # i = 0
+  # j = 0
 
-  for items in Item:
-    size_1 = Item[i][1]
-    i += 1
-    for items in Item:
-      size_2 = Item[j][1]
-      j += 1
-      mid_sum = size_1 + size_2
-    if mid_sum <= capacity and i != j:
-      indice.append(i, j)
+  # for items in Item:
+  #   size_1 = Item[i][1]
+  #   i += 1
+  #   for items in Item:
+  #     size_2 = Item[j][1]
+  #     j += 1
+  #     mid_sum = size_1 + size_2
+
+  #   if mid_sum <= capacity and i != j:
+  #     indice.append(i, j)
+
+  # Momoized recursive
+  """
+  Memoized Recursive Strategy 
+  
+  The idea: we'll use the same naive recursive logic but augment it 
+  with the ability to save work we've already done. This doesn't actually
+  improve the theoretical runtime complexity over the naive recursive 
+  approach, but it does significantly improve the actual running time.
+  
+  1. Initialize a cache (can be a list or dictionary)
+  2. Write a helper function that checks the cache for the answer we're looking for
+  3. If the answer is not found, fall back on our naive logic
+  4. The naive helper needs to recursively call the memoized version, not itself
+  5. Return the value that the memoized helper function finds
+
+def nth_fib_memo(n):
+  cache = {}
+  
+  def nth_fib_memo_helper(n):
+    if n not in cache.keys():
+      cache[n] = naive_nth_fib_helper(n)
+    return cache[n]
+      
+  def naive_nth_fib_helper(n):
+    if n < 2:
+      return n
+    return nth_fib_memo_helper(n - 1) + nth_fib_memo_helper(n - 2)
+  
+  return nth_fib_memo_helper(n)
+  """
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
