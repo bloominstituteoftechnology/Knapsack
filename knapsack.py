decc5655f23c2c85ed40b5e3d396bd80c9874cb4
udykas <udykas@gmail.com>
2018-07-18 17:15:44 -0400

    Got a working function

--- knapsack.py
+++ knapsack.py
@@ -7,16 +7,31 @@ Item = namedtuple('Item', ['index', 'size', 'value', 'true_value'])
 
 def knapsack_solver(items, capacity):
   true_value = 0
+  temp = 0
   array = []
+  result = []
   knapsack = capacity or 100
   for item in items:
     array.append(item.true_value)
 
   array.sort(key=int)
-  # while knapsack > 0:
-  #   pass
+  while knapsack > 0:
+    temp = array.pop()
+    print("Temp", temp)
+    if array == []:
+      break
+    for item in items:
+      if item.true_value == temp:
+        if item.size < knapsack:
+          result.append(item.index)
+          print("Result: ", result)
+          knapsack -= item.size
+        else: 
+          continue
+      else:
+        continue
 
-  return array
+  return result
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
