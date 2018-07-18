fa84555d6fc903b114a144430a2ac17156551906
Zach Campbell <beazlebubbles@gmail.com>
2018-07-18 14:10:44 -0600

    Implemented brute force solution

--- knapsack.py
+++ knapsack.py
@@ -3,24 +3,45 @@
 import sys
 from collections import namedtuple
 
-Item = namedtuple('Item', ['index', 'size', 'value'])
+Item = namedtuple("Item", ["index", "size", "value"])
 
-def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME
-  pass
 
-if __name__ == '__main__':
-  if len(sys.argv) > 1:
-    capacity = int(sys.argv[2])
-    file_location = sys.argv[1].strip()
-    file_contents = open(file_location, 'r')
-    items = []
+def knapsack_solver(items, capacity, index=0, total_value=0, choices=[]):
+    # Utter brute force
+    print(capacity, index, total_value, choices)
+    if index >= len(items):
+        return [total_value, choices]
+    else:
+        if items[index].size <= capacity:
+            choices_copy = choices.copy()
+            choices_copy.append(items[index].index)
+            return max(
+                knapsack_solver(
+                    items,
+                    capacity - items[index].size,
+                    index + 1,
+                    total_value + items[index].value,
+                    choices_copy,
+                ),
+                knapsack_solver(items, capacity, index + 1, total_value, choices),
+                key=lambda i: i[0],
+            )
+        else:
+            return knapsack_solver(items, capacity, index + 1, total_value, choices)
 
-    for line in file_contents.readlines():
-      data = line.rstrip().split()
-      items.append(Item(int(data[0]), int(data[1]), int(data[2])))
-    
-    file_contents.close()
-    print(knapsack_solver(items, capacity))
-  else:
-    print('Usage: knapsack.py [filename] [capacity]')
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        capacity = int(sys.argv[2])
+        file_location = sys.argv[1].strip()
+        file_contents = open(file_location, "r")
+        items = []
+
+        for line in file_contents.readlines():
+            data = line.rstrip().split()
+            items.append(Item(int(data[0]), int(data[1]), int(data[2])))
+
+            file_contents.close()
+            print(knapsack_solver(items, capacity))
+        else:
+            print("Usage: knapsack.py [filename] [capacity]")
