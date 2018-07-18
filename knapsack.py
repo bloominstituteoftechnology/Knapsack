72b8bbd32f26ceafe87134ef2117b7ddb627140a
Obo Agboghidi <oagboghi2@gmail.com>
2018-07-18 14:31:52 -0700

    first idea

--- knapsack.py
+++ knapsack.py
@@ -7,7 +7,28 @@ Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
   # !!!! IMPLEMENT ME
-  pass
+  def check_knapsack(i, size):
+    if items[i].size) > size:
+      return check_knapsack(i - 1, size)
+
+    elif( i == -1):
+      return "Knapsack is empty"
+
+    else: 
+      item1 = check_knapsack(i - 1, size)
+      item2 = check_knapsack(i - 1, size - items[i].size)
+
+      item2.value += items[i].value
+
+      if(item1.value > item2.value):
+        return item1
+
+      else:
+        item2.size += items[i].size
+        item2.chosen = item1.chosen + (i + i)
+        return item2
+    
+    return check_knapsack(len(item) - 1, capacity)
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
