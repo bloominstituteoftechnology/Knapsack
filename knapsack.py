7610dc332cc3432074396b9f9bc4422414beec30
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-07-18 18:55:14 -0400

    Generating posibles combinations

--- knapsack.py
+++ knapsack.py
@@ -7,8 +7,22 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME
-  pass
+  totalCapacity = 0
+  totalValue = 0
+  combination = []
+  solutions = []
+  i = 0
+  while i < len(items):
+    if totalCapacity + items[i].size <= capacity:
+      totalCapacity += items[i].size
+      combination.append(items[i])
+      i += 1
+    else:
+      totalCapacity = 0
+      totalValue = 0
+      solutions.append(combination)
+      combination = []
+  return solutions
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
@@ -22,6 +36,9 @@ if __name__ == '__main__':
       items.append(Item(int(data[0]), int(data[1]), int(data[2])))
     
     file_contents.close()
-    print(knapsack_solver(items, capacity))
+    solutions = knapsack_solver(items, capacity)
+    for solution in solutions:
+      print(solution)
+
   else:
     print('Usage: knapsack.py [filename] [capacity]')
