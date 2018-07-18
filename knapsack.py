3ab731e493cda2f1d1859b065ca4a5f09b18ab5d
Sean Chen <skypemaster007@gmail.com>
2018-07-18 09:43:29 -0500

    Add knapsack.py starter file

--- knapsack.py
+++ knapsack.py
@@ -6,6 +6,7 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
+  # !!!! IMPLEMENT ME
   pass
 
 if __name__ == '__main__':
