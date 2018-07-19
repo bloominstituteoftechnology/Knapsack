9ca29b2a4b9017819519a939c29da35059de2c4a
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-19 16:44:28 -0700

    bruce force done

--- knapsack.py
+++ knapsack.py
@@ -18,7 +18,39 @@ def knapsack_solver(items, capacity):
     size += firstSetItem.size
     value += firstSetItem.value
 
-    
+    for secondSetItem in items:
+      if firstSetItem != secondSetItem and secondSetItem.size + size <= capacity:
+          tuple.append(secondSetItem)
+          size += secondSetItem.size
+          value += secondSetItem.value
+
+    if value > current_value:
+      current_set = tuple
+      current_value = value
+
+  return 'Items to select: {} \n Total cost: {} \n Total value: {}'.format(sorted([item.index for item in current_set]),
+    sum([item.size for item in current_set]), current_value)
+
+def better_knapsack_solver(items, capacity):
+    storage = {}
+    knapsack = {}
+    for weight in range(0, capacity+1):
+        storage[0, weight] = 0
+        knapsack[0, weight] = []
+
+    for index in range(1, len(items)+1):
+        for weight in range(0, capacity+1):
+            if items[index-1].size > weight:
+                storage[index, weight] = storage[index-1, weight]
+                knapsack[index, weight] = knapsack[index-1, weight]
+            else:
+                contains_new = storage[index-1, weight-items[index-1].size] + items[index-1].value
+                contains_no_new = storage[index-1, weight]
+                
+                if contains_new > contains_no_new:
+                    storage[index, weight] = contains_new
+                    knapsack[index, weight] = knapsack[index-1, weight-items[index-1].size] + [index]
+    #not done yet..
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
