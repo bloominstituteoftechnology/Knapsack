30695e73eeeec5dbc14dcd6335ed19db03b62c4f
unknown <sam.park.email@gmail.com>
2018-07-19 18:00:29 -0400

    greedy solver

--- knapsack.py
+++ knapsack.py
@@ -88,30 +88,92 @@ def knapsack_solver(items, capacity):
   return knapsack_helper(items, capacity, 0, [0] * len(items))
 
 
+"""def knapsack_second_pass(items, capacity):
 
+  cache = {}
 
+  def knapsack_memo_helper(items, capacity, value, taken):
+    if taken not in cache.keys():
+      cache[value, taken] = knapsack_second_pass_helper(value, taken)
+    return cache[value, taken]
 
-
-
+  def knapsack_second_pass_helper(value, taken):
+    if not items:
+      return value, taken
+    elif len(items) == 1:
+      #check if the last item fits or not
+      if items[0].size <= capacity:
+        #take the item by setting its index in 'taken' to 1
+        taken[items[0].index -1 ] = 1
+        # 50ish min on video prob
+        #look up .index
+        #update our total value for taking this item
+        value += items[0].value
+        return value, taken
+      else:
+        #last item doesnt fit, just discard it
+        return value, taken
+      # we still have a bunch of items to consider
+      #check to see if the item we just picked up fits in our remaining capacity
+    elif items[0].size <= capacity:
+        # we can consider the overall value of this item if it fits
+        # make a copy of taken
+      taken_copy = taken[:]
+      taken_copy[items[0].index - 1] = 1
+      r1 = knapsack_memo_helper(items[1:], capacity - items[0].size, 
+                            value + items[0].value, taken_copy)
+      r2 = knapsack_memo_helper(items[1:], capacity, value, taken)
+      # r1 is universe where item was taken r2 is universe is where we didnt take
+      #pick the universe that results in the largest value
+      # r1 = (20, [1,0,0,1])
+      # r2 = (19, [1,1,0,0]) 
+      # make sure to compare 20 to 19(resulting values)
+      return max(r1, r2, key=lambda tup: tup[0])
+    else:
+          #item doesnt fit, discard it and continue to next item
+          return knapsack_memo_helper(items[1:], capacity, value, taken)
+    # make initial call to recursive helper
+  return knapsack_memo_helper(items, capacity, 0, [0] * len(items))
   """
-  max_value_list = []
-  print(math.trunc((25 / 68) * 100))
-  print(items)
-  for i, c, v in items:
-    max_val =  i, c, v, (math.trunc((v / c) * 100))
-    max_value_list.append(max_val)
+      
     
-  for i in max_value_list:
-    totalw = 0
-    totalv = 0
-    total_list = []
-    if i[1] < 100:
-      totalv += i[1]
-      totalw += 
 
-    print(total_list)
-  print(max_value_list)
-  """
+
+
+
+
+
+
+def simple_solver(items, capacity):
+  new_items = []
+  if len(items) < 1 or capacity < 1:
+    return 0  
+  for i in items:
+    new_items.append(i)
+  new_items.reverse()
+  print(sorted(new_items, key=lambda value: value[2], reverse=True))
+
+
+################# Greedy Solver ############################
+
+def greedy_solver(items, capacity):
+  max_value_list = []
+  for i, c, v in items:
+    max_val =  i, c, v, (v / float(c))
+    max_value_list.append(max_val) 
+
+  max_value_list = sorted(max_value_list, key=lambda value: value[3], reverse=True)  
+  totalv = 0
+  total_list = []
+  for i in max_value_list:
+    if i[1] <= capacity:
+      totalv += i[2]
+      capacity -= i[1]
+      total_list.append(i[0])
+  return(totalv, total_list)
+
+  
+
 
   
   """
@@ -138,6 +200,7 @@ if __name__ == '__main__':
       items.append(Item(int(data[0]), int(data[1]), int(data[2])))
     
     file_contents.close()
-    print(knapsack_solver(items, capacity))
+    #print(knapsack_solver(items, capacity))
+    print(greedy_solver(items, capacity))
   else:
     print('Usage: knapsack.py [filename] [capacity]')
