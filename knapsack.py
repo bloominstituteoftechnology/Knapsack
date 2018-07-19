6f8302725717f18a364e965d7a6197432a61044b
jesuarva <jesuarva.developer@gmail.com>
2018-07-20 01:32:20 +0200

    MEMORIZATION: Implement thirdAttempt(), an apprach ussing Sets.

--- knapsack.py
+++ knapsack.py
@@ -97,7 +97,9 @@ def knapsack_solver(items, capacity):
                 # print(f'''x: {x[0]}''')
 
                 # Add current Item to the picked up control
-                control['itemsPicked'].append(x[0])
+                control['itemsPicked'].append((x[0]))
+                control['itemsPicked'].sort()
+                # print(string)
                 # print(
                 #     f'''{control['iterations']} Level: {level} - x: {x[0]} - items_picked: {control['itemsPicked']}''')
                 # print('AFTER APPEND ITEM/DEEP\nc:',
@@ -108,7 +110,10 @@ def knapsack_solver(items, capacity):
                 control['combinatories'].append(control['itemsPicked'].copy())
                 control['combinatories'][
                     0] = f'''Total combinations: {len(control['combinatories']) - 1}'''
-                print(f'''c: {control['combinatories'][-1]}\n''')
+                # print(items_passed)
+                # print(f'''c: {control['combinatories'][-1]}\n''')
+                # print(
+                #     f'''\n\tpermutation: {control['combinatories'][-1]}\n\tSize: {MAX['size']}\n\tValue: {MAX['value']}\n\tItems picked: {MAX['items']}\n''')
 
                 # ADD TO BAG LOGIC
                 # Get current_level items' size
@@ -116,10 +121,10 @@ def knapsack_solver(items, capacity):
                 for index in control['itemsPicked']:
                     current_level_size += items[index - 1][1]
 
-                # There is a bug reading the capacity from the command line -> it get incremented always by 1 => So I subtract 1 in the comparison here
-                # False if we want all posible permutations (check next if statement: It prints all posible permutations)
                 remainig_items = [
                     item for item in items_passed if item[0] != x[0]]
+                # There is a bug reading the capacity from the command line -> it get incremented always by 1 => So I subtract 1 in the comparison here
+                # False if we want all posible permutations (check next if statement: It prints all posible permutations)
                 if True and current_level_size <= capacity - 1:
                     # Get current_level items' value
                     current_level_value = 0
@@ -134,6 +139,9 @@ def knapsack_solver(items, capacity):
 
                     # GO TO NEXT LEVEL:
                     # (NESTED FOR LOOP) - Iterate again over the remaining Items
+                    # PRINT CURRENT BAG
+                    # print(
+                    #     f'''\n\tSize: {MAX['size']}\n\tValue: {MAX['value']}\n\tItems picked: {MAX['items']}\n\tc: {control['combinatories'][-1]}\n''')
                     getMAxValue2((level + 1), remainig_items)
 
                 # print('\nItemsPicked', control['itemsPicked'])
@@ -150,13 +158,106 @@ def knapsack_solver(items, capacity):
         getMAxValue2(1, items)
         print(
             f'''\n**********BRUTAL FORCE**********\nCONTROL:\n\tCapacity readed by the default implementation: {capacity}\n\tCapacity passed in the CLI: {capacity - 1}\n\tIterations: {control['iterations']}\n\tPermutations: {control['combinatories'][0]}\n\tSize: {MAX['size']}\n\tValue: {MAX['value']}\n\tItems picked: {MAX['items']}\n\nEND''')
+        # Iterate over ALL posible combinations
 
-        def getMAxValue2_handle_add_to_bag():
-            pass
+    def thirdAttempt(items, capacity):  # BRUTE FORCE
+
+        items_len = len(items)
+        MAX = {
+            'items': [],
+            'size': 0,
+            'value': 0,
+        }
+        control = {
+            'items': [],
+            'size': 0,
+            'value': 0,
+            'itemsPicked': [],
+            'combinatories': set('0'),
+            'iterations': 1,
+        }
+        # control['combinatories'].add('1')
+        # print(control['combinatories'])
+
+        # Iterate over ALL posible combinations
+        def getMAxValue2(level, items_passed):  # WORKING ALGORITHM
+            if level > items_len:
+                # print(
+                #     f'''\n \LEVEL: {level} \DEEP: {control['itemsPicked']} - return''')
+                return
+
+            items_passed_len = len(items_passed)
+
+            # print('\n\n\nITEMS PASSED:', items_passed)
+            # print(len(items_passed))
+            for x in items_passed:
+                # print(items_passed)
+
+                # print('for x in items_passed: x = ', x[0])
+                # MAX['items'], MAX['size'], MAX['value'] = control['itemsPicked'], control['size'], control['value']
+                # print(
+                #     f'''\n\LEVEL: {level} \DEEP: {control['itemsPicked']}''')
+                # # print('c:', control)
+                # print(f'''x: {x[0]}''')
+
+                # Add current Item to the picked up control
+                control['itemsPicked'].append((x[0]))
+                copy = control['itemsPicked'][:]
+                copy.sort()
+                string_of_items_picked = ''.join([str(num) for num in copy])
+                # print(string_of_items_picked)
+
+                # ADD TO BAG LOGIC
+                # Get current_level items' size
+                current_level_size = 0
+                for index in control['itemsPicked']:
+                    current_level_size += items[index - 1][1]
+
+                remainig_items = [
+                    item for item in items_passed if item[0] != x[0]]
+                # There is a bug reading the capacity from the command line -> it get incremented always by 1 => So I subtract 1 in the comparison here
+                # False if we want all posible permutations (check next if statement: It prints all posible permutations)
+                # True: Print all posible Permutations
+                # print(control['combinatories'].__contains__(
+                #     string_of_items_picked))
+                # if control['combinatories'].__contains__(string_of_items_picked):
+                if string_of_items_picked in control['combinatories']:
+                    pass  # The current Combinatorie is in the List of combinatories
+                elif True and current_level_size <= capacity - 1:
+                    control['combinatories'].add(string_of_items_picked)
+
+                    # Get current_level items' value
+                    current_level_value = 0
+                    for index in control['itemsPicked']:
+                        current_level_value += items[index - 1][2]
+
+                    # Compare Level_value with Bag_value
+                    if current_level_value > MAX['value']:
+                        MAX['items'] = control['itemsPicked'].copy()
+                        MAX['size'] = current_level_size
+                        MAX['value'] = current_level_value
+
+                    # GO TO NEXT LEVEL:
+                    # (NESTED FOR LOOP) - Iterate again over the remaining Items
+                    # PRINT CURRENT BAG
+                    # print(
+                    #     f'''\n\tSize: {MAX['size']}\n\tValue: {MAX['value']}\n\tItems picked: {MAX['items']}\n\tc: {control['combinatories']}\n''')
+                    getMAxValue2((level + 1), remainig_items)
+
+                if x[0] in control['itemsPicked']:
+                    control['itemsPicked'].remove(x[0])
+
+        getMAxValue2(1, items)
+        print(
+            f'''\n**********MEMORIZATION**********\nCONTROL:\n\tCapacity readed by the default implementation: {capacity}\n\tCapacity passed in the CLI: {capacity - 1}\n\tIterations: {control['iterations']}\n\tCombinatories: {len(control['combinatories'])}\n\tSize: {MAX['size']}\n\tValue: {MAX['value']}\n\tItems picked: {MAX['items']}\n\nEND''')
+        # Iterate over ALL posible combinations
 
     start_time = time.clock()
-    secondAttempt(items, capacity + 1)
+    thirdAttempt(items, capacity + 1)
     print(f'''\n\nTime runned: {(time.clock() - start_time) / 60} minutes''')
+    # start_time = time.clock()
+    # secondAttempt(items, capacity + 1)
+    # print(f'''\n\nTime runned: {(time.clock() - start_time) / 60} minutes''')
 
 
 if __name__ == '__main__':
