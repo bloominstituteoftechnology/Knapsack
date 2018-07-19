1fd20964d7f66cf4a4b6facb147aba6188147a61
Usman Javed <ujaved931@gmail.com>
2018-07-20 04:23:51 +0500

    trying bu iterative

--- knapsack.py
+++ knapsack.py
@@ -6,7 +6,7 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 # taken = [0] * len(items)
-
+items = []
 
 def knapsack_solver(items, capacity):
     def knapsack_helper(items, capacity, value, taken):
@@ -31,19 +31,42 @@ def knapsack_solver(items, capacity):
             return knapsack_helper(items[1:], capacity, value, taken)
     return knapsack_helper(items, capacity, 0, [0] * len(items))
 
+def knapsack_iterative(items, capacity):
+  """
+  1. initialize a cache as a matrix where the x axis is item index and y axis is capcity
+  2. seed the matrix with values
+  3. pick out the items that we'll take in our knapsack
+  4.
+  """
+  # initializing our matrix cache
+  cache = [[0] * (capacity + 1) for _ in range(len(items)+1)]
+  #initialize taken
+  taken = set()
 
+  #populate our matrix
+  for item in range(1, len(cache)):
+    for size in range(len(cache[item])):
+      #decide if we're going to take this item or not
+      if item[item-1].size > size:
+        cache[item][size] = cache[item-1][size]
+      else:
+        # we take the item
+        r1 = cache[item-1][size]
+        r2 = cache[item-1][size-items[item-1].size + items[item-1].value]
+        cache[item][size] = max(r1, r2)
+        
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        capacity = int(sys.argv[2])
-        file_location = sys.argv[1].strip()
-        file_contents = open(file_location, 'r')
-        items = []
+  if len(sys.argv) > 1:
+    capacity = int(sys.argv[2])
+    file_location = sys.argv[1].strip()
+    file_contents = open(file_location, 'r')
+    items = []
 
-        for line in file_contents.readlines():
-            data = line.rstrip().split()
-            items.append(Items(int(data[0]), int(data[1]), int(data[2])))
-
-        file_contents.close()
-        print(knapsack_solver(items, capacity))
-    else:
-        print('Usage: knapsack.py [filename] [capacity]')
+    for line in file_contents.readlines():
+      data = line.rstrip().split()
+      items.append(Item(int(data[0]), int(data[1]), int(data[2])))
+    
+    file_contents.close()
+    print(knapsack_solver(items, capacity))
+  else:
+    print('Usage: knapsack.py [filename] [capacity]')
