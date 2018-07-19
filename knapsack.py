1e0c5b543c64024cef535cbac3bd63c600150110
Obo Agboghidi <oagboghi2@gmail.com>
2018-07-19 13:48:41 -0700

    greedy appraoch

--- knapsack.py
+++ knapsack.py
@@ -1,34 +1,72 @@
 #!/usr/bin/python
 
 import sys
+import decimal
 from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
+#Greedy approach
+
 def knapsack_solver(items, capacity):
+  sortRatio = []
+  fullStats = []
+  empArr = []
+  size_counter = int(0)
   # !!!! IMPLEMENT ME
-  def check_knapsack(i, size):
-    if items[i].size > size:
-      return check_knapsack(i - 1, size)
 
-    elif( i == -1):
-      return "Knapsack is empty"
+  d = {'key':'value'}
+  d['mynewkey'] = 'mynewvalue'
+  print(d)
 
-    else: 
-      item1 = check_knapsack(i - 1, size)
-      item2 = check_knapsack(i - 1, size - items[i].size)
+  print("test")
+  for i in items:
+    ratio = i.value / float(i.size)
+    itemsObject = {
+      'size':i.size, 
+      'value':i.value,
+      'ratio': float(ratio)}
+    sorted(itemsObject)
+    fullStats.append(itemsObject)
+  for elements in fullStats:
+    if fullStats[elements]['ratio'] > 1:
+      empArr.append(fullStats[elements])
+    else:
+     continue
+  print(empArr)
+  
+#################
+# taken = [0] = len(items)
 
-      item2.value += items[i].value
+# def knapsack_solver(items, capacity):
+#   # Recursively checking all combinations of items
+#   # inputs: items, capacity, total value, taken items
+#   # returns the resulting value and the taken array of taken items
+#   def knapsack_helper(items, capacity, value, taken):
+#     if not items:
+#       return value, taken
+#     elif len(items) == 1:
+#       # Check if the last item fits or not
+#       if items[0].size <= capacity:
+#         # take the item by setting its index to 'taken' to 1
+#         taken[items[0].index - 1] = 1
+#         # update our total value for taking this item
+#         value += items[0].value
+#         return value, taken
+#       else:
+#         #last item doesn't fit, just discard it
+#         return value, taken
+#     # end of base cases
+#     # check to see if the item we just pickedup fits
 
-      if(item1.value > item2.value):
-        return item1
 
-      else:
-        item2.size += items[i].size
-        item2.chosen = item1.chosen + (i + i)
-        return item2
     
-    #return check_knapsack(len(item) - 1, capacity)
+    
+    
+
+      
+    
+  #print("ratio ", sortRatio)
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
