f018db6c88f70db90cba7292971e9a5e9ad67029
Tramane Hall <tramanedhall@gmail.com>
2018-07-19 19:08:02 -0400

    Second attempt at solving problem

--- knapsack.py
+++ knapsack.py
@@ -6,23 +6,42 @@ from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
+'''
+Greedy Strategy
+  1. Go through our items and filter out any items whose size > knapsack's capacity
+  2. 'Score' each item by determining its value/weight ratio
+  3. Sort the items array by each item's ratio such that the items with the best ratio
+  are at the top of the array of items
+  4. Grab items off the top of the items array until we reach our knapsack's full capacity
+
+'''
+
 def knapsack_solver(items, capacity):
-  best_haul = None
+  best_haul = []
   highest_value = 0
 
-  for FirstItem in items:
-    haul = []
-    size = 0 
-    value = 0
+  for item in items:
+     Prehaul = []
+     for FirstItem in items:
+      haul = []
+      size = 0 
+      value = 0
 
-    haul.append(FirstItem)
-    size += FirstItem.size
-    value += FirstItem.value
+      haul.append(FirstItem)
+      size += FirstItem.size
+      value += FirstItem.value
+      print(size, value)
+     
+      if value > highest_value:
+        #if FirstItem != haul[0]:
+          if size <= capacity:
+            best_haul = haul
+            Prehaul.append(haul[0])
+            highest_value = value
 
-    if value > highest_value:
-      best_haul = haul
-      highest_value = value
+  return best_haul
 
+'''
     for SecondItem in items:
       if FirstItem != SecondItem:
         if SecondItem.size + size <= capacity:
@@ -33,8 +52,7 @@ def knapsack_solver(items, capacity):
     if value > highest_value:
       best_haul = haul
       highest_value = value
-
-  
+'''  
 
 
 if __name__ == '__main__':
