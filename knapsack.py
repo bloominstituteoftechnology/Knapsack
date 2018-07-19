e64cde9151e39472f34cdeda25ef2500a6ecf07c
Antonio Hernandez <antonio@antoniohernandez.me>
2018-07-18 21:52:39 -0500

    Brute force algorithm is working

--- knapsack.py
+++ knapsack.py
@@ -7,7 +7,45 @@ Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
   # !!!! IMPLEMENT ME
-  pass
+  #print(items[:][-1])
+  sorted_list = ratio_list(items)
+  print(sorted_list)
+ 
+  # Base case
+  if len(items) == 0 or capacity == 0:
+    return 0
+
+  counter = 0 
+  knapsack = []
+
+  print(sorted_list[counter][2])
+
+  while (capacity > 0) and (counter < len(sorted_list)):
+    if capacity >= int(sorted_list[counter][2]):
+      knapsack.append(int(sorted_list[counter][0]))
+      capacity -= int(sorted_list[counter][2])
+      print("capacity is ", capacity, "counter is ", counter)
+    counter += 1
+  
+  return sorted(knapsack)
+
+def ratio_list(items):
+  number_of_items = len(items)
+  sorted_list = [] * number_of_items
+
+  for i in range(number_of_items):
+    # Index, ratio, and size
+    sorted_list.append([items[i][0], round((items[i][2] / items[i][1]),2), items[i][1]]) # Round was used to limit decimals to 2
+
+  # Use the ratio of value/size and place the most convenient item at beginning of the list
+  sorted_list = sorted(sorted_list, key=get_key, reverse=True)
+  #print(sorted_list)
+  return sorted_list
+
+ 
+def get_key(item):
+  return item[1]
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
