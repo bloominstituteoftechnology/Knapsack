70828a54555546597f5eaea002923fae92c096e2
kammagik <kammagik@yahoo.com>
2018-07-19 15:49:49 -0700

    erased extra empty array

--- knapsack.py
+++ knapsack.py
@@ -6,7 +6,7 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  knapsack = []
+  
   
   # # if knapsack if full
   # if capacity == 0:
