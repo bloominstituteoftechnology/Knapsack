f2cd78c4acff038d402e7d37bb0a181189bb2e7e
JSNavey <jsnavey@gmail.com>
2018-07-18 23:31:38 -0400

    try to approach the problem by using subset but no luck

--- knapsack.py
+++ knapsack.py
@@ -4,11 +4,38 @@ import sys
 from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
+#                 item_set  e[0],    e[1],    e[2]
 
-def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME
-  # well...it seems difficult 
-  pass
+def knapsack_solver(items, capacity, value, size):
+  '''
+  # return subset but not the knapsack answer
+
+  # initialize the placeholders
+  knapsack = []
+  best_value = 0
+  max_capacity = 0
+
+  # find the subset
+  def powerset(items):
+    res = []
+    for item in items:
+      newset = [r+[item] for r in res]
+      res.extend(newset)
+    return res
+
+  print('powerset', items)
+
+  # sum weight and value through each subset
+  # update the weight and value when it finds the best value and less weight
+  for item_set in powerset(items):
+    set_capacity = sum([e[1] for e in item_set])
+    set_value = sum([e[2] for e in item_set])
+    if set_value > best_value and set_capacity <= max_capacity:
+      best_value = set_value
+      max_capacity = set_capacity
+      knapsack = item_set
+  return knapsack, best_value, max_capacity
+'''
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
