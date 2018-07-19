fbe4a59ee1594074f51df55b43b87de9e2ca926b
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-07-19 12:26:49 -0700

    implemented solution code for recursive method

--- knapsack.py
+++ knapsack.py
@@ -6,9 +6,27 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME
-  pass
-
+  def ks_helper(items, capacity, value, bag):
+    if not items:
+      return value, bag
+    elif len(items) == 1:
+      if items[0].size <= capacity:
+        bag[items[0].index - 1] = 1
+        value += items[0].value
+        return value, bag
+      else:
+        return value, bag
+    elif items[0].size <= capacity:
+      bag_copy = bag[:]
+      bag_copy[items[0].index - 1] = 1
+      branch_1 = ks_helper(items[1:], capacity - items[0].size,
+                           value + items[0].value, bag_copy)
+      branch_2 = ks_helper(items[1:], capacity, value, bag)
+      return max(branch_1, branch_2, key=lambda tup: tup[0])
+    else:
+      return ks_helper(items[1:], capacity, value, bag)
+  return ks_helper(items, capacity, 0, [0] * len(items))
+  
 if __name__ == '__main__':
   if len(sys.argv) > 1:
     capacity = int(sys.argv[2])
@@ -21,6 +39,6 @@ if __name__ == '__main__':
       items.append(Item(int(data[0]), int(data[1]), int(data[2])))
     
     file_contents.close()
-    print(knapsack_solver(items, capacity))
+    print("Recursive: ", knapsack_solver(items, capacity))
   else:
     print('Usage: knapsack.py [filename] [capacity]')
