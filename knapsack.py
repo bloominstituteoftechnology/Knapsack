775398b0d77dc7ee7ed7301458925da28468c56d
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-07-20 08:26:48 -0700

    solution for iterative method

--- knapsack.py
+++ knapsack.py
@@ -26,7 +26,34 @@ def knapsack_solver(items, capacity):
     else:
       return ks_helper(items[1:], capacity, value, bag)
   return ks_helper(items, capacity, 0, [0] * len(items))
+
+def ks_iterative(items, capacity):
+  cache = [[0] * (capacity + 1) for _ in range(len(items) + 1)]
+  bag = set()
+
+  for item in range(1, len(cache)):
+    for size in range(len(cache[item])):
+      if items[items-1].size > size:
+        cache[item][size] = cache[item-1][size]
+      else:
+        branch_1 = cache[item-1][size]
+        branch_2 = cache[item-1][size - items[item-1].size] + items[item-1].value
+        cache[item][size] = max(branch_1, branch_2)
   
+  rows = len(cache) - 1
+  cols = len(cache[0]) - 1
+
+  while rows > 0 and col > 0:
+    if cache[rows][cols] != cache[rows-1][cols]:
+      bag.add(rows - 1)
+      rows -= 1
+      cols -= items[rows].size
+    else:
+      rows -= 1
+  
+  return cache[-1][-1], bag
+  return ks_iterative(items, capacity)
+
 if __name__ == '__main__':
   if len(sys.argv) > 1:
     capacity = int(sys.argv[2])
@@ -40,5 +67,7 @@ if __name__ == '__main__':
     
     file_contents.close()
     print("Recursive: ", knapsack_solver(items, capacity))
+    # print("Memoization: ", memo_knap(items, capacity))
+    print("Iterative: ", ks_iterative(items, capacity))
   else:
     print('Usage: knapsack.py [filename] [capacity]')
