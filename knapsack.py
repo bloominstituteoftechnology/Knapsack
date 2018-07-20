40407b128ad427cf5c776891f4afd732b11d7afd
Antonio Hernandez <antonio@antoniohernandez.me>
2018-07-19 21:03:45 -0500

    Cleaned code a little

--- knapsack.py
+++ knapsack.py
@@ -16,28 +16,26 @@ def knapsack_solver(items, capacity):
     return 0
 
   index = 0  
-  size = 0
-  value = 0
-  knapsack = []
+  knapsack = {'indeces':[]  , 'value': 0, 'size': 0}
 
   while (capacity > 0) and (index < len(sorted_list)):
     if capacity >= int(sorted_list[index][2]):
-      knapsack.append(int(sorted_list[index][0])) # Append the itemem
-      capacity -= int(sorted_list[index][2])  # subtract item size from the capacity y 
-      size += sorted_list[index][2] # Add item sizeze
-      value += items[sorted_list[index][0]].value # Add the value of item found by using the index from sorted_listst
-      print("capacity is ", capacity, "index is ", index)
+      knapsack['indeces'].append(int(sorted_list[index][0])) # Append the item index
+      knapsack['size'] += sorted_list[index][2] # Add item size
+      knapsack['value'] += items[sorted_list[index][0]].value # Add the value of item found by using the index from sorted_list
+      capacity -= int(sorted_list[index][2])  # subtract item size from the capacity  
+      #print("capacity is ", capacity, "index is ", index) # Used for debugging somehow my value result does not match the result.
     index += 1 
-  
-  print('Value', value,'\n', 'Size', size)
-  return sorted(knapsack)
+
+  print('Value:', knapsack['value'], '\nSize:', knapsack['size'], '\nChosen:')
+  return (sorted(knapsack['indeces']))
 
 def ratio_list(items):
   number_of_items = len(items)
   sorted_list = [] * number_of_items
 
   for i in range(number_of_items):
-    # Index, ratio, and size
+    # The sorted_list contains index, ratio(value/size), and size; from items
     sorted_list.append([items[i][0], round((items[i][2] / items[i][1]),2), items[i][1]]) # Round was used to limit decimals to 2
 
   # Use the ratio of value/size and place the most convenient item at beginning of the list
@@ -61,12 +59,13 @@ if __name__ == '__main__':
   else:
     print('Usage: knapsack.py [filename] [capacity]')
 
+'''The gunner list was taken from the chosen output in the greedy section of the large1.txt file.
+This test still shows the same outcome as my result which differs from the result in greedy algorithm of
+the large1.txt file '''
+
 #gunner = [671,104,737,370,432,239,107,935,561,297,796,134,693,83,949,704,271,782,814,866,566,420,295,795,997,44,648,844,623,160,337,907,909,329,335,308,373,913,700]
-##gunner = [38,39,40,41,42,43,44,45,46,47,48,49]
-#
-#suma = 0
+#summa = 0
 #for i in gunner:
 #  print(i, items[i].value)
-#  suma += items[i].value
-#  print('suma', suma)
-#print(suma)
+#  summa += items[i].value
+#  print('summa', summa)
