3f19b0e6f8d41a41d2eb2610ee05626cecdf6b55
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-22 21:27:19 -0500

    Struggling with this one...

--- knapsack.py
+++ knapsack.py
@@ -12,16 +12,17 @@ def knapsack_solver(items, capacity):
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
-    capacity = int(sys.argv[2])
-    file_location = sys.argv[1].strip()
+    capacity = int(sys.argv[2]) # how much can fit in the knapsack
+    file_location = sys.argv[1].strip() # file with items/weights/values
     file_contents = open(file_location, 'r')
     items = []
 
     for line in file_contents.readlines():
-      data = line.rstrip().split()
+      data = line.rstrip().split() # rstrip() strips off parameter (default newlines)
       items.append(Item(int(data[0]), int(data[1]), int(data[2])))
     
     file_contents.close()
     print(knapsack_solver(items, capacity))
   else:
     print('Usage: knapsack.py [filename] [capacity]')
+
