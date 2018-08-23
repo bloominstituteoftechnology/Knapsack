e29617819f4236ec4997dbef64ed8ef02c458070
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-23 16:15:15 -0500

    Stretching my brain so far it feels like it's going to snap; greedy version a work in progress.

--- knapsack.py
+++ knapsack.py
@@ -6,14 +6,25 @@ from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value', 'ratio'])
 
+# okay so let's try to implement the brute-force version
+# I don't know why but I keep going in circles with this one
+# 
+
+
+
+# greedy version--seems to work
 def knapsack_solver(items, capacity):
     items_sorted_by_ratios = (sorted(items, key=lambda x: x.ratio))
     knapsack = []
+    total_value = 0
     avail_weight = capacity
+    
     for i in reversed(items_sorted_by_ratios):
         if i.size <= avail_weight:
           knapsack.append(i)
           avail_weight -= i.size
+          total_value += i.value
+    knapsack.append(total_value)
     return knapsack
 
 
@@ -28,9 +39,9 @@ if __name__ == '__main__':
 
     for line in file_contents.readlines():
       data = line.rstrip().split()
-      items.append(Item(int(data[0]), int(data[1]), int(data[2]), int(data[1]) / int(data[2])))
+      items.append(Item(int(data[0]), int(data[1]), int(data[2]), int(data[2]) / int(data[1])))
     
     file_contents.close()
-    print(knapsack_solver(items, capacity))
+    print("Here are the contents of your knapsack: ", knapsack_solver(items, capacity))
   else:
     print('Usage: knapsack.py [filename] [capacity]')
