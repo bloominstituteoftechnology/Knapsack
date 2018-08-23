1c3bad8f97f48deda433f1e2cb9030f07452d28e
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-23 15:16:44 -0500

    Still struggling but have the ratios now.

--- knapsack.py
+++ knapsack.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python
-#okay
 
 import sys
 from collections import namedtuple
 
-Item = namedtuple('Item', ['index', 'size', 'value'])
+Item = namedtuple('Item', ['index', 'size', 'value', 'ratio'])
 
 def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME
-  # brute force attempt
-  # accept that there may never be a 'later'
-  max_capacity = False
-  total_value = 0
-
+  return items
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
-    capacity = int(sys.argv[2]) # how much can fit in the knapsack
-    file_location = sys.argv[1].strip() # file with items/weights/values
+    capacity = int(sys.argv[2])
+    file_location = sys.argv[1].strip()
     file_contents = open(file_location, 'r')
     items = []
 
     for line in file_contents.readlines():
-      data = line.rstrip().split() # rstrip() strips off parameter (default newlines)
-      items.append(Item(int(data[0]), int(data[1]), int(data[2])))
+      data = line.rstrip().split()
+      items.append(Item(int(data[0]), int(data[1]), int(data[2]), int(data[1]) / int(data[2])))
     
     file_contents.close()
     print(knapsack_solver(items, capacity))
   else:
     print('Usage: knapsack.py [filename] [capacity]')
-
